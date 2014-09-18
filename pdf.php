35ab0ae33b0fd8b253f03ac37e891264947668f7
Jake Jackson <admin@blueliquiddesigns.com.au>
2014-09-18 11:42:14 +1000

    Push version update

--- pdf.php
+++ pdf.php
@@ -4,7 +4,7 @@
 Plugin Name: Gravity Forms PDF Extended
 Plugin URI: http://www.gravityformspdfextended.com
 Description: Gravity Forms PDF Extended allows you to save/view/download a PDF from the front- and back-end, and automate PDF creation on form submission. Our Business Plus package also allows you to overlay field onto an existing PDF.
-Version: 3.5.7
+Version: 3.5.8
 Author: Blue Liquid Designs
 Author URI: http://www.blueliquiddesigns.com.au
 
@@ -33,7 +33,7 @@ GNU General Public License for more details.
 /*
  * Define our constants 
  */
-define('PDF_EXTENDED_VERSION', '3.5.7'); 
+define('PDF_EXTENDED_VERSION', '3.5.8'); 
 define('GF_PDF_EXTENDED_SUPPORTED_VERSION', '1.7'); 
 define('GF_PDF_EXTENDED_WP_SUPPORTED_VERSION', '3.5'); 
 define('GF_PDF_EXTENDED_PHP_SUPPORTED_VERSION', '5'); 
--- view/templates/settings/changelog.php
+++ view/templates/settings/changelog.php
@@ -22,6 +22,11 @@
 
   <h3><?php echo PDF_EXTENDED_VERSION; ?></h3>
   <ul>
+    <li>Fixed issue affected some users where a depreciated function was causing a fatal error </li>
+  </ul>
+  
+  <h3>3.5.7</h3>
+  <ul>
     <li>Bug - Fixed issue where the PDF settings page was blank for some users</li>
   </ul>
 
