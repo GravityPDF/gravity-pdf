830155bfd552c0efe21b7c52042a385451fe2d25
Jake Jackson <admin@blueliquiddesigns.com.au>
2015-09-01 18:59:12 +1000

    Add package.json and gulp CSS minify and JS minify script

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+
+var gulp  	  = require('gulp'),
+	minifyCss = require('gulp-minify-css'),
+	uglify    = require('gulp-uglify'),
+	rename    = require('gulp-rename');
+
+/* Minify our CSS */
+gulp.task('minify', function() {
+  return gulp.src(['src/assets/css/*.css', '!src/assets/css/*.min.css'])
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(rename({
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest('src/assets/css/'));
+});
+
+/* Minify our JS */
+gulp.task('compress', function() {
+  return gulp.src(['src/assets/js/*.js', 'src/assets/js/*.min.js'])
+    .pipe(uglify())
+    .pipe(rename({
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest('src/assets/js/'));
+});
+
+gulp.task('default', function() {
+    gulp.start('minify', 'compress');
+});
