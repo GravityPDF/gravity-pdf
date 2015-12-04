47c6e2e217e5bbd20f42760112b4451c0062cd94
Jake Jackson <admin@blueliquiddesigns.com.au>
2015-12-04 13:52:53 +1100

    Optimise List and Product Styles
    - Move Product and List field styles our of their associated helper classes and place them in our core template style loader (which contains our header/footer and background code).
    - Change the background and border colours to match the dynamic background colour being used using hex colour manipulation
    
    Resolves #58

--- src/views/html/PDF/core_template_styles.php
+++ src/views/html/PDF/core_template_styles.php
@@ -14,20 +14,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-$font             = ( ! empty( $settings['font'] ) ) ? $settings['font'] : 'DejavuSansCondensed';
-$font_colour      = ( ! empty( $settings['font_colour'] ) ) ? $settings['font_colour'] : '#333';
-$font_size        = ( ! empty( $settings['font_size'] ) ) ? $settings['font_size'] : '9';
+global $gfpdf;
 
-$header           = ( ! empty( $settings['header'] ) ) ? $settings['header'] : '';
-$footer           = ( ! empty( $settings['footer'] ) ) ? $settings['footer'] : '';
-$first_header     = ( ! empty( $settings['first_header'] ) ) ? $settings['first_header'] : '';
-$first_footer     = ( ! empty( $settings['first_footer'] ) ) ? $settings['first_footer'] : '';
+$font                       = ( ! empty( $settings['font'] ) ) ? $settings['font'] : 'DejavuSansCondensed';
+$font_colour                = ( ! empty( $settings['font_colour'] ) ) ? $settings['font_colour'] : '#333';
+$font_size                  = ( ! empty( $settings['font_size'] ) ) ? $settings['font_size'] : '9';
 
-$background_color = ( ! empty( $settings['background_color'] ) ) ? $settings['background_color'] : '';
-$background_image = ( ! empty( $settings['background_image'] ) ) ? $settings['background_image'] : '';
+$header                     = ( ! empty( $settings['header'] ) ) ? $settings['header'] : '';
+$footer                     = ( ! empty( $settings['footer'] ) ) ? $settings['footer'] : '';
+$first_header               = ( ! empty( $settings['first_header'] ) ) ? $settings['first_header'] : '';
+$first_footer               = ( ! empty( $settings['first_footer'] ) ) ? $settings['first_footer'] : '';
+
+$background_color           = ( ! empty( $settings['background_color'] ) ) ? $settings['background_color'] : '#FFF';
+$background_image           = ( ! empty( $settings['background_image'] ) ) ? $settings['background_image'] : '';
+
+$background_color_contrast  = $gfpdf->misc->get_contrast( $background_color );
+$contrast_value             = ( $background_color_contrast === '#FFF' ) ? 20 : -20;
+$contrast_background_color  = $gfpdf->misc->change_brightness( $background_color, $contrast_value );
+$border_contrast            = ( $background_color_contrast === '#FFF' ) ? 60 : -60;
+$reverse_border_contrast    = ( $background_color_contrast === '#FFF' ) ? '#000' : '#FFF';
+$contrast_border_color      = $gfpdf->misc->change_brightness( $reverse_border_contrast, $border_contrast );
+
+$include_list_styles        = apply_filters( 'gfpdf_include_list_styles', true );
+$include_product_styles     = apply_filters( 'gfpdf_include_product_styles', true );
 
 ?>
-
 
 <style>
 	@page {
@@ -54,15 +65,15 @@ $background_image = ( ! empty( $settings['background_image'] ) ) ? $settings['ba
 	}
 
 	@page :first {
-		<?php if ( ! empty($first_header) ) : ?>
-			header: html_TemplateFirstHeader;
-			margin-header: 5mm;
-		<?php endif; ?>
+	<?php if ( ! empty($first_header) ) : ?>
+		header: html_TemplateFirstHeader;
+		margin-header: 5mm;
+	<?php endif; ?>
 
-		<?php if ( ! empty($first_footer) ) : ?>
-			footer: html_TemplateFirstFooter;
-			margin-footer: 5mm;
-		<?php endif; ?>
+	<?php if ( ! empty($first_footer) ) : ?>
+		footer: html_TemplateFirstFooter;
+		margin-footer: 5mm;
+	<?php endif; ?>
 	}
 
 	body, table th, table td, ul li, ol li {
@@ -75,6 +86,82 @@ $background_image = ( ! empty( $settings['background_image'] ) ) ? $settings['ba
 		width: auto !important;
 		max-height: 25mm;
 	}
+
+	/* List Field Styles */
+	<?php if ( $include_product_styles ) : ?>
+		.gfield_list {
+			border-collapse: collapse;
+			border: 1px solid <?php echo $contrast_border_color; ?>;
+			margin: 2px 0 6px;
+			padding: 0;
+			width: 100%;
+		}
+
+		.gfield_list th {
+			text-align: left;
+			background-color: <?php echo $contrast_background_color; ?>;
+			border: 1px solid <?php echo $contrast_border_color; ?>;
+			font-weight: bold;
+			padding: 6px 10px;
+		}
+
+		.gfield_list td {
+			padding: 6px 10px;
+			border: 1px solid <?php echo $contrast_border_color; ?>;
+		}
+	<?php endif; ?>
+
+	/* Product Field Styles */
+	<?php if ( $include_product_styles ) : ?>
+		table.entry-products th {
+			background-color: <?php echo $contrast_background_color; ?>#F4F4F4;
+			border-bottom: 1px solid <?php echo $contrast_border_color; ?>;
+			border-right: 1px solid <?php echo $contrast_border_color; ?> !important;
+		}
+
+		table.entry-products td.textcenter, table.entry-products th.textcenter {
+			text-align: center;
+		}
+
+		table.entry-products col.entry-products-col2 {
+			width: 50px;
+		}
+
+		table.entry-products col.entry-products-col3 {
+			width: 155px;
+		}
+
+		table.entry-products col.entry-products-col4 {
+			width: 155px;
+		}
+
+		table.entry-products {
+			border: 1px solid <?php echo $contrast_border_color; ?>;
+			margin: 10px 0;
+		}
+
+		table.entry-products td {
+			border-bottom: 1px solid <?php echo $contrast_border_color; ?>;
+			border-right: 1px solid <?php echo $contrast_border_color; ?> !important;
+			padding: 7px 7px 8px;
+			vertical-align: top;
+		}
+
+		table.entry-products td.emptycell {
+			background-color: <?php echo $contrast_background_color; ?>;
+		}
+
+		table.entry-products td.totals {
+			font-size: 13px;
+			font-weight: bold;
+			padding-bottom: 8px;
+			padding-top: 7px;
+		}
+
+		table.entry-products td.textright, table.entry-products th.textright {
+			text-align: right;
+		}
+	<?php endif; ?>
 
 </style>
 
