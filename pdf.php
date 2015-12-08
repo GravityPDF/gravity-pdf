76b7d9c9714866c4a35cb9a2ac1bffa18cf838e9
Jake Jackson <jake@gravitypdf.com>
2015-12-08 12:22:12 +1100

    Bump our WP minimum requirement
    Resolves #110

--- pdf.php
+++ pdf.php
@@ -103,7 +103,7 @@ class GFPDF_Major_Compatibility_Checks {
 	 *
 	 * @since 4.0
 	 */
-	public $required_wp_version = '3.9';
+	public $required_wp_version = '4.0';
 
 	/**
 	 * The plugin's required PHP version
