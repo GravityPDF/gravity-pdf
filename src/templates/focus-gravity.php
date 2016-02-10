56c9e1d92fa0cd2c139450733e7160d3f5043913
Jake Jackson <jake@gravitypdf.com>
2016-02-10 15:56:21 +1100

    Pass in the current template's initialised config class
    This allows devs to move their core template logic into reusable code blocks with ease.
    
    Fixes #259

--- src/templates/focus-gravity.php
+++ src/templates/focus-gravity.php
@@ -23,6 +23,7 @@ if ( ! class_exists('GFForms')) {
  * $form_data (The processed entry data stored in an array)
  * $settings (the current PDF configuration)
  * $fields (an array of Gravity Form fields which can be accessed with their ID number)
+ * $config (The initialised template config class – /config/focus-gravity.php)
  * $gfpdf (the main Gravity PDF object containing all our helper classes)
  * $args (contains an array of all variables - the ones being described right now - passed to the template)
  */
