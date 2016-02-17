7ec85772372f670bddc935758b7361c254ad2d31
Jake Jackson <jake@gravitypdf.com>
2016-02-17 13:35:51 +1100

    Change shortcode 'classes' to 'class' but leave in backwards compat support
    Fixes #273

--- src/view/html/Shortcodes/gravitypdf.php
+++ src/view/html/Shortcodes/gravitypdf.php
@@ -36,6 +36,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 
-<a href="<?php echo $args['url']; ?>" class="<?php echo $args['classes']; ?>">
-	<?php echo $args['text']; ?>
-</a>
+<a href="<?php echo $args['url']; ?>" class="<?php echo $args['class']; ?> <?php echo $args['classes']; ?>"><?php echo $args['text']; ?></a>
