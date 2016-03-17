GravityPDF/gravity-pdf

commit 0ca3a762def22c8470d107712e190928be61743a

Preliminary fix for Tier 2 add-on.
There was a major issue with the way we were parsing the legacy Tier 2 add-on. It appeared to work correctly when only one Tier 2 template was being run but once you started using multiples it fell to pieces.

This patch reimagines how the legacy Tier 2 compatibility should function in Gravity PDF 4.0. Instead of handling it inside our Helper_PDF class, we are instead bypassing that functionality and doing the checks before hand in our Model_PDF and View_PDF functions (one for saving and one for viewing).

We also fixed a clean-up issue when resending PDFs via the admin area.

This patch still needs both unit testing and more hands on testing before merging.

Resolves #289

Add unit tests and optimise code

src/model/Model_PDF.php
@@ -750,21 +750,36 @@ class Model_PDF extends Helper_Abstract_Model {
 			/* Get required parameters */
 			$entry    = $pdf->get_entry();
 			$settings = $pdf->get_settings();
+			$args     = $this->misc->get_template_args( $entry, $settings );
 
 			/* Add backwards compatibility support */
 			$GLOBALS['wp']->query_vars['pid'] = $settings['id'];
 			$GLOBALS['wp']->query_vars['lid'] = $entry['id'];
 
 			try {
-				$pdf->init();
-				$pdf->set_output_type( 'save' );
-				$pdf->render_html( $this->misc->get_template_args( $entry, $settings ) );
 
-				/* Generate PDF if PDF doesn't already exists (backwards compatibility support for v3 Tier 2) */
-				if ( ! $this->does_pdf_exist( $pdf ) ) {
-					$raw_pdf = $pdf->generate();
-					$pdf->save_pdf( $raw_pdf );
+				/* Initialise our PDF helper class */
+				$pdf->init();
+				$pdf->set_template();
+				$pdf->set_output_type( 'save' );
+
+				/* Increment our rudimentary PDF counter */
+				$this->options->increment_pdf_count();
+
+				/* Add Backwards compatibility support for our v3 Tier 2 Add-on */
+				if ( isset( $settings['advanced_template'] ) && strtolower( $settings['advanced_template'] ) == 'yes' ) {
+
+					/* Check if we should process this document using our legacy system */
+					if ( $this->handle_legacy_tier_2_processing( $pdf, $entry, $settings, $args ) ) {
+						return true;
+					}
 				}
+
+				/* Render the PDF template HTML */
+				$pdf->render_html( $args );
+
+				/* Generate and save the PDF */
+				$pdf->save_pdf( $pdf->generate() );
 
 				return true;
 			} catch ( Exception $e ) {
@@ -779,6 +794,35 @@ class Model_PDF extends Helper_Abstract_Model {
 		}
 
 		return true;
+	}
+
+	/**
+	 * Handles the loading and running of our legacy Tier 2 PDF templates
+	 *
+	 * @param \GFPDF\Helper\Helper_PDF $pdf      The Helper_PDF object
+	 * @param array                    $entry    The Gravity Forms raw entry data
+	 * @param array                    $settings The Gravity PDF settings
+	 * @param array                    $args     The data that should be passed directly to a PDF template
+	 *
+	 * @return bool
+	 *
+	 * @since 4.0
+	 */
+	public function handle_legacy_tier_2_processing( Helper_PDF $pdf, $entry, $settings, $args ) {
+
+		$form = $this->form->get_form( $entry['form_id'] );
+
+		$prevent_main_pdf_loader = apply_filters( 'gfpdfe_pre_load_template',
+			$form['id'],
+			$entry['id'],
+			basename( $pdf->get_template_path() ),
+			$form['id'] . $entry['id'],
+			$this->misc->backwards_compat_output( $pdf->get_output_type() ),
+			$pdf->get_filename(),
+			$this->misc->backwards_compat_conversion( $settings )
+		); /* Backwards Compatibility */
+
+		return ( $prevent_main_pdf_loader === true ) ? true : false;
 	}
 
 	/**
@@ -839,21 +883,17 @@ class Model_PDF extends Helper_Abstract_Model {
 			$notifications['attachments'] = ( isset( $notifications['attachments'] ) ) ? $notifications['attachments'] : array();
 
 			/* Loop through each PDF config and generate */
-			foreach ( $pdfs as $pdf_config ) {
+			foreach ( $pdfs as $settings ) {
 
-				$settings = $this->options->get_pdf( $entry['form_id'], $pdf_config['id'] );
+				/* Reset the variables each loop */
+				$filename = $tier_2_filename = '';
 
-				if ( ! is_wp_error( $settings ) && $this->maybe_attach_to_notification( $notifications, $settings ) ) {
+				if ( $this->maybe_attach_to_notification( $notifications, $settings ) ) {
 
-					/* Add backwards compatibility filter for our Tier 2 v3 Package if needed */
-					if( isset( $settings['advanced_template'] ) && strtolower( $settings['advanced_template'] ) == 'yes' ) {
-						$tier_2_filename = apply_filters( 'gfpdfe_return_pdf_path', $form['id'], $entry['id'] );
-					}
-
-					$filename = ( isset( $tier_2_filename ) && is_file( $tier_2_filename ) ) ? $tier_2_filename : $this->generate_and_save_pdf( $entry, $settings );
+					/* Generate our PDF */
+					$filename = $this->generate_and_save_pdf( $entry, $settings );
 
 					if ( ! is_wp_error( $filename ) ) {
-						$this->options->increment_pdf_count();
 						$notifications['attachments'][] = $filename;
 					}
 				}
@@ -953,7 +993,7 @@ class Model_PDF extends Helper_Abstract_Model {
 	}
 
 	/**
-	 * To prevent out tmp directory getting huge we will clean it up every 24 hours
+	 * To prevent ourt tmp directory getting huge we will clean it up every 24 hours
 	 *
 	 * @return void
 	 *
@@ -1036,6 +1076,25 @@ class Model_PDF extends Helper_Abstract_Model {
 				}
 			}
 		}
+	}
+
+	/**
+	 * Clean-up any PDFs stored on disk before we resend any notifications
+	 *
+	 * @param array $form The Gravity Forms object
+	 * @param array $leads An array of Gravity Form entry IDs
+	 *
+	 * @since 4.0
+	 *
+	 * @return array We tapped into a filter so we need to return the form object
+	 */
+	public function resend_notification_pdf_cleanup( $form, $leads ) {
+		foreach ( $leads as $entry_id ) {
+			$entry = $this->form->get_entry( $entry_id );
+			$this->cleanup_pdf( $entry, $form );
+		}
+
+		return $form;
 	}
 
 	/**

src/controller/Controller_PDF.php
@@ -147,9 +147,9 @@ class Controller_PDF extends Helper_Abstract_Controller implements Helper_Interf
 
 		/* Add save PDF filter */
 		add_action( 'gform_after_submission', array( $this->model, 'maybe_save_pdf' ), 10, 2 );
-		add_action( 'gform_after_submission', array( $this->model, 'cleanup_pdf' ), 9999, 2 );
 
-		/* Setup clean-up cron */
+		/* Clean-up actions */
+		add_action( 'gform_after_submission', array( $this->model, 'cleanup_pdf' ), 9999, 2 );
 		add_action( 'gfpdf_cleanup_tmp_dir', array( $this->model, 'cleanup_tmp_dir' ) );
 	}
 
@@ -194,6 +194,9 @@ class Controller_PDF extends Helper_Abstract_Controller implements Helper_Interf
 		/* Pre-process our template arguments and automatically render them in PDF */
 		add_filter( 'gfpdf_template_args', array( $this->model, 'preprocess_template_arguments' ) );
 		add_filter( 'gfpdf_pdf_html_output', array( $this->view, 'autoprocess_core_template_options' ), 5, 4 );
+
+		/* Cleanup filters */
+		add_filter( 'gform_before_resend_notifications', array( $this->model, 'resend_notification_pdf_cleanup' ), 10, 2 );
 	}
 
 	/**

src/helper/Helper_Misc.php
@@ -967,4 +967,52 @@ class Helper_Misc {
 		return $fields;
 	}
 
+	/**
+	 * Converts the 4.x settings array into a compatible 3.x settings array
+	 *
+	 * @param  array $settings The 4.x settings to be converted
+	 *
+	 * @return array           The 3.x compatible settings
+	 *
+	 * @since 4.0
+	 */
+	public function backwards_compat_conversion( $settings ) {
+
+		$compat                   = array();
+		$compat['premium']        = ( isset( $settings['advanced_template'] ) && $settings['advanced_template'] == 'Yes' ) ? true : false;
+		$compat['rtl']            = ( isset( $settings['rtl'] ) && $settings['rtl'] == 'Yes' ) ? true : false;
+		$compat['dpi']            = ( isset( $settings['image_dpi'] ) ) ? (int) $settings['image_dpi'] : 96;
+		$compat['security']       = ( isset( $settings['security'] ) && $settings['security'] == 'Yes' ) ? true : false;
+		$compat['pdf_password']   = ( isset( $settings['password'] ) ) ? $settings['password'] : '';
+		$compat['pdf_privileges'] = ( isset( $settings['privileges'] ) ) ? $settings['privileges'] : '';
+		$compat['pdfa1b']         = ( isset( $settings['format'] ) && $settings['format'] == 'PDFA1B' ) ? true : false;
+		$compat['pdfx1a']         = ( isset( $settings['format'] ) && $settings['format'] == 'PDFX1A' ) ? true : false;
+
+		return $compat;
+	}
+
+	/**
+	 * Converts the 4.x output to into a compatible 3.x type
+	 *
+	 * @param  string $type
+	 *
+	 * @return string
+	 *
+	 * @since 4.0
+	 */
+	public function backwards_compat_output( $type = '' ) {
+		switch ( strtolower( $type ) ) {
+			case 'display':
+				return 'view';
+			break;
+
+			case 'download':
+				return 'download';
+			break;
+
+			default:
+				return 'save';
+			break;
+		}
+	}
 }

src/helper/Helper_PDF.php
@@ -214,13 +214,6 @@ class Helper_PDF {
 
 		$form = $this->form->get_form( $this->entry['form_id'] );
 
-		/* Add filter to prevent HTML being written to document when it returns true. Backwards compatibility. */
-		$prevent_html = apply_filters( 'gfpdfe_pre_load_template', $form['id'], $this->entry['id'], basename( $this->template_path ), $form['id'] . $this->entry['id'], $this->backwards_compat_output( $this->output ), $this->filename, $this->backwards_compat_conversion( $this->settings ), $args ); /* Backwards Compatibility */
-
-		if ( $prevent_html === true ) {
-			return;
-		}
-
 		/* Load in our PHP template */
 		if ( empty( $html ) ) {
 			$html = $this->load_html( $args );
@@ -318,6 +311,65 @@ class Helper_PDF {
 	}
 
 	/**
+	 * Get the correct path to the PHP template we should load into mPDF
+	 *
+	 * @throws Exception
+	 *
+	 * @since 4.0
+	 */
+	public function set_template() {
+
+		$template = ( isset( $this->settings['template'] ) ) ? $this->get_file_with_extension( $this->settings['template'] ) : '';
+
+		/* Allow a user to change the current template if they have the appropriate capabilities */
+		if ( rgget( 'template' ) && is_user_logged_in() && $this->form->has_capability( 'gravityforms_edit_settings' ) ) {
+			$template = $this->get_file_with_extension( rgget( 'template' ) );
+		}
+
+		/**
+		 * Check for the template's existance
+		 * We'll first look for a user-overridding template
+		 * Then check our default templates
+		 */
+		$default_template_path = PDF_PLUGIN_DIR . 'src/templates/';
+
+		if ( is_multisite() && is_file( $this->data->multisite_template_location . $template ) ) {
+			$this->template_path = $this->data->multisite_template_location . $template;
+		} else if ( is_file( $this->data->template_location . $template ) ) {
+			$this->template_path = $this->data->template_location . $template;
+		} else if ( is_file( $default_template_path . $template ) ) {
+			$this->template_path = $default_template_path . $template;
+		} else {
+			throw new Exception( 'Could not find the template: ' . esc_html( $template ) );
+		}
+
+		/*
+		 * Check if the template version is compatible with our version of Gravity PDF
+		 */
+		$headers = get_file_data( $this->template_path, array( 'required_pdf_version' => __( 'Required PDF Version', 'gravity-forms-pdf-extended' ) ) );
+
+		/* Check if there are version requirements */
+		if ( strlen( $headers['required_pdf_version'] ) > 0 ) {
+			/* Check if the version requirements are NOT met and throw and error */
+			if ( version_compare( $headers['required_pdf_version'], PDF_EXTENDED_VERSION, '>' ) ) {
+				throw new Exception( sprintf( __( 'The PDF Template %s requires Gravity PDF version %s. Upgrade to the latest version.', 'gravity-forms-pdf-extended' ), "<em>$template</em>", "<em>{$headers['required_pdf_version']}</em>" ) );
+			}
+		}
+	}
+
+	/**
+	 * Gets the current directory template files are being included from.
+	 * This is set in the set_template() method
+	 *
+	 * @return string
+	 *
+	 * @since 4.0
+	 */
+	public function get_template_path() {
+		return $this->template_path;
+	}
+
+	/**
 	 * Public endpoint to allow users to control how the generated PDF will be displayed
 	 *
 	 * @param string $type Only display, download or save options are valid
@@ -334,6 +386,18 @@ class Helper_PDF {
 		}
 
 		$this->output = strtoupper( $type );
+	}
+
+
+	/**
+	 * Get the current PDF output type as per the set_output_type() method.
+	 *
+	 * @return string
+	 *
+	 * @since 4.0
+	 */
+	public function get_output_type() {
+		return $this->output;
 	}
 
 	/**
@@ -503,6 +567,9 @@ class Helper_PDF {
 	/**
 	 * Gets the absolute path to the PDF
 	 *
+	 * Works with our legacy Tier 2 add-on without adding a filter because we have stuck with the same naming convension
+	 *
+	 *
 	 * @return string The full path and filename of the PDF
 	 *
 	 * @since 4.0
@@ -641,54 +708,6 @@ class Helper_PDF {
 		}
 	}
 
-	/**
-	 * Get the correct path to the PHP template we should load into mPDF
-	 *
-	 * @throws Exception
-	 *
-	 * @since 4.0
-	 */
-	protected function set_template() {
-
-		$template = ( isset( $this->settings['template'] ) ) ? $this->get_file_with_extension( $this->settings['template'] ) : '';
-
-		/* Allow a user to change the current template if they have the appropriate capabilities */
-		if ( rgget( 'template' ) && is_user_logged_in() && $this->form->has_capability( 'gravityforms_edit_settings' ) ) {
-			$template = $this->get_file_with_extension( rgget( 'template' ) );
-		}
-
-		/**
-		 * Check for the template's existance
-		 * We'll first look for a user-overridding template
-		 * Then check our default templates
-		 */
-		$default_template_path = PDF_PLUGIN_DIR . 'src/templates/';
-
-		if ( is_multisite() && is_file( $this->data->multisite_template_location . $template ) ) {
-			$this->template_path = $this->data->multisite_template_location . $template;
-		} else if ( is_file( $this->data->template_location . $template ) ) {
-			$this->template_path = $this->data->template_location . $template;
-		} else if ( is_file( $default_template_path . $template ) ) {
-			$this->template_path = $default_template_path . $template;
-		} else {
-			throw new Exception( 'Could not find the template: ' . esc_html( $template ) );
-		}
-
-		/*
-		 * Check if the template version is compatible with our version of Gravity PDF
-		 */
-		$headers = get_file_data( $this->template_path, array( 'required_pdf_version' => __( 'Required PDF Version', 'gravity-forms-pdf-extended' ) ) );
-
-		/* Check if there are version requirements */
-		if ( strlen( $headers['required_pdf_version'] ) > 0 ) {
-			/* Check if the version requirements are NOT met and throw and error */
-			if ( version_compare( $headers['required_pdf_version'], PDF_EXTENDED_VERSION, '>' ) ) {
-				throw new Exception( sprintf( __( 'The PDF Template %s requires Gravity PDF version %s. Upgrade to the latest version.', 'gravity-forms-pdf-extended' ), "<em>$template</em>", "<em>{$headers['required_pdf_version']}</em>" ) );
-			}
-		}
-
-	}
-
 
 	/**
 	 * Ensure an extension is added to the end of the name
@@ -825,55 +844,6 @@ class Helper_PDF {
 			$master_password = ( isset( $this->settings['master_password'] ) ) ? $this->settings['master_password'] : '';
 
 			$this->mpdf->SetProtection( $privileges, $password, $master_password, 128 );
-		}
-	}
-
-	/**
-	 * Converts the 4.x settings array into a compatible 3.x settings array
-	 *
-	 * @param  array $settings The 4.x settings to be converted
-	 *
-	 * @return array           The 3.x compatible settings
-	 *
-	 * @since 4.0
-	 */
-	protected function backwards_compat_conversion( $settings ) {
-
-		$compat                   = array();
-		$compat['premium']        = ( isset( $settings['advanced_template'] ) && $settings['advanced_template'] == 'Yes' ) ? true : false;
-		$compat['rtl']            = ( isset( $settings['rtl'] ) && $settings['rtl'] == 'Yes' ) ? true : false;
-		$compat['dpi']            = ( isset( $settings['image_dpi'] ) ) ? (int) $settings['image_dpi'] : 96;
-		$compat['security']       = ( isset( $settings['security'] ) && $settings['security'] == 'Yes' ) ? true : false;
-		$compat['pdf_password']   = ( isset( $settings['password'] ) ) ? $settings['password'] : '';
-		$compat['pdf_privileges'] = ( isset( $settings['privileges'] ) ) ? $settings['privileges'] : '';
-		$compat['pdfa1b']         = ( isset( $settings['format'] ) && $settings['format'] == 'PDFA1B' ) ? true : false;
-		$compat['pdfx1a']         = ( isset( $settings['format'] ) && $settings['format'] == 'PDFX1A' ) ? true : false;
-
-		return $compat;
-	}
-
-	/**
-	 * Converts the 4.x output to into a compatible 3.x type
-	 *
-	 * @param  string $type
-	 *
-	 * @return string
-	 *
-	 * @since 4.0
-	 */
-	protected function backwards_compat_output( $type ) {
-		switch ( strtolower( $type ) ) {
-			case 'display':
-				return 'view';
-			break;
-
-			case 'download':
-				return 'download';
-			break;
-
-			default:
-				return 'save';
-			break;
 		}
 	}
 

src/view/View_PDF.php
@@ -188,21 +188,36 @@ class View_PDF extends Helper_Abstract_View {
 		$pdf->set_filename( $model->get_pdf_name( $settings, $entry ) );
 
 		try {
-			$pdf->init();
 
-			/* set display type */
+			/* Initialise our PDF helper class */
+			$pdf->init();
+			$pdf->set_template();
+
+			/* Increment our rudimentary PDF counter */
+			$this->options->increment_pdf_count();
+
+			/* Set display type and allow user to override the behaviour */
 			$settings['pdf_action'] = apply_filters( 'gfpdfe_pdf_output_type', $settings['pdf_action'] ); /* Backwards compat */
 			if ( $settings['pdf_action'] == 'download' ) {
 				$pdf->set_output_type( 'download' );
 			}
 
-			/* determine if we should show the print dialog box */
+			/* Add Backwards compatibility support for our v3 Tier 2 Add-on */
+			if ( isset( $settings['advanced_template'] ) && strtolower( $settings['advanced_template'] ) == 'yes' ) {
+
+				/* Check if we should process this document using our legacy system */
+				if ( $model->handle_legacy_tier_2_processing( $pdf, $entry, $settings, $args ) ) {
+					return true;
+				}
+			}
+
+			/* Determine if we should show the print dialog box */
 			if ( isset( $_GET['print'] ) ) {
 				$pdf->set_print_dialog( true );
 			}
 
+			/* Render the PDF template HTML */
 			$pdf->render_html( $args );
-			$this->options->increment_pdf_count();
 
 			/* Generate PDF */
 			$pdf->generate();

tests/phpunit/unit-tests/test-helper-misc.php
@@ -7,7 +7,7 @@ use GFPDF\Helper\Helper_Misc;
 use WP_UnitTestCase;
 
 /**
- * Test Gravity PDF Hlper Misc Functionality
+ * Test Gravity PDF Helper Misc Functionality
  *
  * @package     Gravity PDF
  * @copyright   Copyright (c) 2015, Blue Liquid Designs
@@ -462,11 +462,11 @@ class Test_Helper_Misc extends WP_UnitTestCase {
 	 * Ensure we correctly return an appropriate class name based on the file path given
 	 *
 	 * @param string $expected The expected value
-	 * @param string $file The test path
+	 * @param string $file     The test path
 	 *
 	 * @dataProvider provider_get_config_class_name
 	 *
-	 * @since 4.0
+	 * @since        4.0
 	 */
 	public function test_get_config_class_name( $expected, $file ) {
 		$this->assertEquals( $expected, $this->misc->get_config_class_name( $file ) );
@@ -497,7 +497,7 @@ class Test_Helper_Misc extends WP_UnitTestCase {
 	 *
 	 * @dataProvider provider_get_background_and_border_contrast
 	 *
-	 * @since 4.0
+	 * @since        4.0
 	 */
 	public function test_get_background_and_border_contrast( $expected, $hex ) {
 		$contrast = $this->misc->get_background_and_border_contrast( $hex );
@@ -535,12 +535,65 @@ class Test_Helper_Misc extends WP_UnitTestCase {
 		$this->assertSame( 0, sizeof( $this->misc->get_fields_sorted_by_id( 0 ) ) );
 
 		/* Check for real form and verify the results */
-		$form  = $GLOBALS['GFPDF_Test']->form['all-form-fields'];
+		$form = $GLOBALS['GFPDF_Test']->form['all-form-fields'];
 
 		$fields = $this->misc->get_fields_sorted_by_id( $form['id'] );
 
 		$this->assertEquals( 54, sizeof( $fields ) );
 		$this->assertEquals( 'Section Break', $fields[10]->label );
+	}
 
+	/**
+	 * Check if our backwards compatible settings conversion works correctly
+	 */
+	public function test_backwards_compat_conversion() {
+		$settings = array(
+			'irrelivant' => 'Yes',
+		);
+
+		/* Check all the defaults work as expected */
+		$compat = $this->misc->backwards_compat_conversion( $settings );
+
+		$this->assertEquals( 8, sizeof( $compat ) );
+		$this->assertFalse( isset( $compat['irrelivant'] ) );
+		$this->assertFalse( $compat['premium'] );
+		$this->assertFalse( $compat['rtl'] );
+		$this->assertFalse( $compat['security'] );
+		$this->assertFalse( $compat['pdfa1b'] );
+		$this->assertFalse( $compat['pdfx1a'] );
+		$this->assertEquals( '', $compat['password'] );
+		$this->assertEquals( '', $compat['pdf_privileges'] );
+		$this->assertEquals( 96, $compat['dpi'] );
+
+		/* Check all the settings get correctly converted */
+		$settings = array(
+			'advanced_template' => 'Yes',
+			'rtl'               => 'Yes',
+			'image_dpi'         => 300,
+			'security'          => 'Yes',
+			'password'          => 'password',
+			'privileges'        => 'privileges',
+			'format'            => 'PDFX1A',
+		);
+
+		$compat = $this->misc->backwards_compat_conversion( $settings );
+
+		$this->assertTrue( $compat['premium'] );
+		$this->assertTrue( $compat['rtl'] );
+		$this->assertTrue( $compat['security'] );
+		$this->assertFalse( $compat['pdfa1b'] );
+		$this->assertTrue( $compat['pdfx1a'] );
+		$this->assertEquals( 'password', $compat['pdf_password'] );
+		$this->assertEquals( 'privileges', $compat['pdf_privileges'] );
+		$this->assertEquals( 300, $compat['dpi'] );
+	}
+
+	/**
+	 * Check if our backwards compatible output functions work correctly
+	 */
+	public function test_backwards_compat_output() {
+		$this->assertEquals( 'save', $this->misc->backwards_compat_output() );
+		$this->assertEquals( 'view', $this->misc->backwards_compat_output( 'display' ) );
+		$this->assertEquals( 'download', $this->misc->backwards_compat_output( 'download' ) );
 	}
 }

commit f9654722905e6c67f58900889d447fc2d9755cc1

Fix Tier 2 regression introduced in commit 0ca3a762def22c8470d107712e190928be61743a
Correct the DocBlock code for our Tier 2 filter

src/depreciated.php
@@ -167,8 +167,8 @@ class PDFRender extends GFPDF_Depreciated_Abstract {
 	 * @param integer $id        The spliced form ID and entry ID
 	 * @param string  $output    The PDF output method
 	 * @param string  $filename  The PDF filename
-	 * @param array   $arguments Any additional arguments to be passed
-	 * @param array   $args      The same as $arguments
+	 * @param array   $arguments The v3 arguments that get passed to the template
+	 * @param array   $args      The v4 arguments that get passed to the template
 	 *
 	 * @since 4.0
 	 *

src/model/Model_PDF.php
@@ -819,7 +819,8 @@ class Model_PDF extends Helper_Abstract_Model {
 			$form['id'] . $entry['id'],
 			$this->misc->backwards_compat_output( $pdf->get_output_type() ),
 			$pdf->get_filename(),
-			$this->misc->backwards_compat_conversion( $settings )
+			$this->misc->backwards_compat_conversion( $settings ),
+			$args
 		); /* Backwards Compatibility */
 
 		return ( $prevent_main_pdf_loader === true ) ? true : false;