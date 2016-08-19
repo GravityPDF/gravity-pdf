e97df446f0c6ae4eb46fa6b3357ac3ceea443071
Jake Jackson <jake@gravitypdf.com>
2016-08-19 16:27:17 +1000

    Update our phpunit bash script to the latest version from WP CLI

--- tests/bin/install.sh
+++ tests/bin/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ $# -lt 3 ]; then
-	echo "usage: $0 <db-name> <db-user> <db-pass> [db-host] [wp-version]"
+	echo "usage: $0 <db-name> <db-user> <db-pass> [db-host] [wp-version] [skip-database-creation]"
 	exit 1
 fi
 
@@ -10,9 +10,10 @@ DB_USER=$2
 DB_PASS=$3
 DB_HOST=${4-localhost}
 WP_VERSION=${5-latest}
+SKIP_DB_CREATE=${6-false}
 
-WP_TESTS_DIR="${PWD}/tmp/wordpress-tests-lib"
-WP_CORE_DIR="${PWD}/tmp/wordpress/"
+WP_TESTS_DIR=${WP_TESTS_DIR-/tmp/wordpress-tests-lib}
+WP_CORE_DIR=${WP_CORE_DIR-/tmp/wordpress/}
 
 download() {
     if [ `which curl` ]; then
@@ -24,6 +25,8 @@ download() {
 
 if [[ $WP_VERSION =~ [0-9]+\.[0-9]+(\.[0-9]+)? ]]; then
 	WP_TESTS_TAG="tags/$WP_VERSION"
+elif [[ $WP_VERSION == 'nightly' || $WP_VERSION == 'trunk' ]]; then
+	WP_TESTS_TAG="trunk"
 else
 	# http serves a single offer, whereas https serves multiple. we only want one
 	download http://api.wordpress.org/core/version-check/1.7/ /tmp/wp-latest.json
@@ -36,11 +39,6 @@ else
 	WP_TESTS_TAG="tags/$LATEST_VERSION"
 fi
 
-#The unit testing suite in 3.9 doesn't handle cleanups correctly. Replace with the 4.0 version
-if [[ $WP_TESTS_TAG == "tags/3.9" ]]; then
-	WP_TESTS_TAG="tags/4.0"
-fi
-
 set -ex
 
 install_wp() {
@@ -51,14 +49,20 @@ install_wp() {
 
 	mkdir -p $WP_CORE_DIR
 
-	if [ $WP_VERSION == 'latest' ]; then
-		local ARCHIVE_NAME='latest'
+	if [[ $WP_VERSION == 'nightly' || $WP_VERSION == 'trunk' ]]; then
+		mkdir -p /tmp/wordpress-nightly
+		download https://wordpress.org/nightly-builds/wordpress-latest.zip  /tmp/wordpress-nightly/wordpress-nightly.zip
+		unzip -q /tmp/wordpress-nightly/wordpress-nightly.zip -d /tmp/wordpress-nightly/
+		mv /tmp/wordpress-nightly/wordpress/* $WP_CORE_DIR
 	else
-		local ARCHIVE_NAME="wordpress-$WP_VERSION"
+		if [ $WP_VERSION == 'latest' ]; then
+			local ARCHIVE_NAME='latest'
+		else
+			local ARCHIVE_NAME="wordpress-$WP_VERSION"
+		fi
+		download https://wordpress.org/${ARCHIVE_NAME}.tar.gz  /tmp/wordpress.tar.gz
+		tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
 	fi
-
-	download https://wordpress.org/${ARCHIVE_NAME}.tar.gz  /tmp/wordpress.tar.gz
-	tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
 
 	download https://raw.github.com/markoheijnen/wp-mysqli/master/db.php $WP_CORE_DIR/wp-content/db.php
 }
@@ -66,23 +70,23 @@ install_wp() {
 install_depencency() {
 	#Gravity Forms
 	wget -nv -O /tmp/gravityforms.tar.gz https://github.com/GravityPDF/gravityforms/archive/master.tar.gz
-	mkdir -p $PWD/tmp/gravityforms
-	tar --strip-components=1 -zxf /tmp/gravityforms.tar.gz -C $PWD/tmp/gravityforms
+	mkdir -p ${WP_CORE_DIR-/tmp/gravityforms}
+	tar --strip-components=1 -zxf /tmp/gravityforms.tar.gz -C ${WP_CORE_DIR-/tmp/gravityforms}
 
 	#Gravity Forms Polls
 	wget -nv -O /tmp/gravityformspolls.tar.gz https://github.com/GravityPDF/gravityformspolls/archive/master.tar.gz
-	mkdir -p $PWD/tmp/gravityformspoll
-	tar --strip-components=1 -zxf /tmp/gravityformspolls.tar.gz -C $PWD/tmp/gravityformspoll
+	mkdir -p ${WP_CORE_DIR-/tmp/gravityformspoll}
+	tar --strip-components=1 -zxf /tmp/gravityformspolls.tar.gz -C ${WP_CORE_DIR-/tmp/gravityformspoll}
 
 	#Gravity Forms Quiz
 	wget -nv -O /tmp/gravityformsquiz.tar.gz https://github.com/GravityPDF/gravityformsquiz/archive/master.tar.gz
-	mkdir -p $PWD/tmp/gravityformsquiz
-	tar --strip-components=1 -zxf /tmp/gravityformsquiz.tar.gz -C $PWD/tmp/gravityformsquiz
+	mkdir -p ${WP_CORE_DIR-/tmp/gravityformsquiz}
+	tar --strip-components=1 -zxf /tmp/gravityformsquiz.tar.gz -C ${WP_CORE_DIR-/tmp/gravityformsquiz}
 
 	#Gravity Forms Survey
 	wget -nv -O /tmp/gravityformssurvey.tar.gz https://github.com/GravityPDF/gravityformssurvey/archive/master.tar.gz
-	mkdir -p $PWD/tmp/gravityformssurvey
-	tar --strip-components=1 -zxf /tmp/gravityformssurvey.tar.gz -C $PWD/tmp/gravityformssurvey
+	mkdir -p ${WP_CORE_DIR-/tmp/gravityformssurvey}
+	tar --strip-components=1 -zxf /tmp/gravityformssurvey.tar.gz -C ${WP_CORE_DIR-/tmp/gravityformssurvey}
 }
 
 install_test_suite() {
@@ -100,11 +104,11 @@ install_test_suite() {
 		svn co --quiet https://develop.svn.wordpress.org/${WP_TESTS_TAG}/tests/phpunit/includes/ $WP_TESTS_DIR/includes
 	fi
 
-	cd $WP_TESTS_DIR
-
 	if [ ! -f wp-tests-config.php ]; then
 		download https://develop.svn.wordpress.org/${WP_TESTS_TAG}/wp-tests-config-sample.php "$WP_TESTS_DIR"/wp-tests-config.php
-		sed $ioption "s:dirname( __FILE__ ) . '/src/':'$WP_CORE_DIR':" "$WP_TESTS_DIR"/wp-tests-config.php
+		# remove all forward slashes in the end
+		WP_CORE_DIR=$(echo $WP_CORE_DIR | sed "s:/\+$::")
+		sed $ioption "s:dirname( __FILE__ ) . '/src/':'$WP_CORE_DIR/':" "$WP_TESTS_DIR"/wp-tests-config.php
 		sed $ioption "s/youremptytestdbnamehere/$DB_NAME/" "$WP_TESTS_DIR"/wp-tests-config.php
 		sed $ioption "s/yourusernamehere/$DB_USER/" "$WP_TESTS_DIR"/wp-tests-config.php
 		sed $ioption "s/yourpasswordhere/$DB_PASS/" "$WP_TESTS_DIR"/wp-tests-config.php
@@ -114,6 +118,11 @@ install_test_suite() {
 
 
 install_db() {
+
+	if [ ${SKIP_DB_CREATE} = "true" ]; then
+		return 0
+	fi
+
 	# parse DB_HOST for port or socket references
 	local PARTS=(${DB_HOST//\:/ })
 	local DB_HOSTNAME=${PARTS[0]};
