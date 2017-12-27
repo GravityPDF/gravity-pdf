e6863247536d859b8eb1cc88801185eca67f2ccd
Jake Jackson <jake@gravitypdf.com>
2017-12-28 09:50:34 +1100

    Fix up spacing problem

--- src/helper/fields/Field_Products.php
+++ src/helper/fields/Field_Products.php
@@ -292,8 +292,8 @@ class Field_Products extends Helper_Abstract_Fields {
 			foreach ( $products['products'] as $id => $product ) {
 
 				if ( class_exists( 'GP_Ecommerce_Fields' ) && empty( $product['name'] ) ) {
-				    continue;
-                }
+					continue;
+				}
 
 				/* Get the raw pricing data */
 				$product_raw_price  = GFCommon::to_number( $product['price'] );
