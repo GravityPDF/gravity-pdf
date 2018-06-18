dc28dcd62f0f56422024a3919ae52488596e0e60
Jake Jackson <jake@gravitypdf.com>
2018-06-18 13:55:32 +1000

    Ensure Async tasks only load when the global setting is enabled

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -875,7 +875,7 @@ class Router implements Helper\Helper_Interface_Actions, Helper\Helper_Interface
 		$model_pdf = $this->singleton->get_class( 'Model_PDF' );
 		$class = new Controller\Controller_Pdf_Queue( $queue, $model_pdf, $this->log );
 
-		if ( $this->options->get_option( 'background_processing', 'Disable' ) === 'Disable' ) {
+		if ( $this->options->get_option( 'background_processing', 'Disable' ) === 'Enable' ) {
 			$class->init();
 		}
 
