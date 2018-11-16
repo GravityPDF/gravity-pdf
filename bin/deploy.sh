8e4d10ed3cb20aa7a03ff309bb2e4f6f5b19647e
Jake Jackson <jake@gravitypdf.com>
2018-11-16 12:36:01 +1100

    Just deploy new tags. Don't deploy trunk.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -33,38 +33,6 @@ cd "$PLUGIN_BUILDS_PATH"
 # Checkout the SVN repo
 svn co -q "http://svn.wp-plugins.org/$PLUGIN" svn
 
-# Move out the trunk directory to a temp location
-mv svn/trunk svn-trunk
-# Create trunk directory
-mkdir svn/trunk
-# Copy our new version of the plugin into trunk
-rsync -r -p $TRAVIS_TAG/* svn/trunk
-
-# Copy all the .svn folders from the checked out copy of trunk to the new trunk.
-# This is necessary as the Travis container runs Subversion 1.6 which has .svn dirs in every sub dir
-cd svn/trunk/
-TARGET=$(pwd)
-cd "$PLUGIN_BUILDS_PATH/svn-trunk"
-
-# Find all .svn dirs in sub dirs
-SVN_DIRS=`find . -type d -iname .svn`
-
-for SVN_DIR in $SVN_DIRS; do
-    SOURCE_DIR=${SVN_DIR/.}
-    TARGET_DIR=$TARGET${SOURCE_DIR/.svn}
-    TARGET_SVN_DIR=$TARGET${SVN_DIR/.}
-    if [ -d "$TARGET_DIR" ]; then
-        # Copy the .svn directory to trunk dir
-        cp -r $SVN_DIR $TARGET_SVN_DIR
-    fi
-done
-
-# Back to builds dir
-cd "$PLUGIN_BUILDS_PATH"
-
-# Remove checked out dir
-rm -fR svn-trunk
-
 # Add new version tag
 mkdir svn/tags/$TRAVIS_TAG
 rsync -r -p $TRAVIS_TAG/* svn/tags/$TRAVIS_TAG
