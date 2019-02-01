33eb1f11fab14171534bbe98a6dada0bc0aa67a1
Debbie Ellis <debbieellis7@gmail.com>
2019-02-01 15:07:22 +0800

    Wrap component for redux provider store

--- src/assets/js/react/bootstrap/helpBootstrap.js
+++ src/assets/js/react/bootstrap/helpBootstrap.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { getStore } from '../store'
 import HelpContainer from '../components/Help/HelpContainer'
 
 export default function helpBootstrap() {
+  const store = getStore()
 
   render(
-    <HelpContainer />,
+    <Provider store={store}>
+      <HelpContainer />
+    </Provider>,
     document.getElementById('search-knowledgebase')
   )
 }
