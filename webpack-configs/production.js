3900d837cf2df20a4d51b0dd46cfa46cf2fbe7d6
Debbie Ellis <debbieellis7@gmail.com>
2019-06-05 13:43:17 +0800

    New webpack config for development and production

---
+++ webpack-configs/production.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  devtool: false,
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new UglifyJSPlugin({
+      parallel: true,
+      sourceMap: true,
+      uglifyOptions: {
+        output: {
+          comments: false
+        }
+      }
+    }),
+    new webpack.SourceMapDevToolPlugin({
+      filename: '[name].js.map',
+      exclude: ['gfpdf-backbone.min.js', 'gfpdf-entries.min.js', 'gfpdf-migration.min.js', 'gfpdf-settings.min.js']
+    })
+  ]
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,45 @@
-const webpack = require('webpack')
+const webpackMerge = require('webpack-merge')
 const path = require('path')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const PROD = (process.env.NODE_ENV === 'production')
+const production = require('./webpack-configs/production')
+const development = require('./webpack-configs/development')
+const modeConfig = PROD ? production : development
 
-module.exports = {
-  entry: {
-    'app.bundle': './src/assets/js/react/gfpdf-main.js',
-    'gfpdf-backbone': './src/assets/js/gfpdf-backbone.js',
-    'gfpdf-entries': './src/assets/js/gfpdf-entries.js',
-    'gfpdf-migration': './src/assets/js/gfpdf-migration.js',
-    'gfpdf-settings': './src/assets/js/gfpdf-settings.js'
-  },
-  output: {
-    path: __dirname + '/dist/assets/js/',
-    filename: '[name].min.js'
-  },
-  mode: PROD ? 'production' : 'development',
-  devtool: PROD ? 'source-map' : 'eval-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: [
-          path.resolve(__dirname, "src/assets"),
-          path.resolve(__dirname, "tests/mocha"),
-        ],
-        loader: 'babel-loader',
-        options: { babelrc: true }
-      },
-      {
-        type: "javascript/auto",
-        test: /\.json$/,
-        loader: 'json-loader'
-      }
-    ]
-  },
-  externals: {
-    'jquery': 'jQuery',
-  },
-
-  plugins: PROD ? [
-    new webpack.DefinePlugin({
-        'process.env': {
-            'NODE_ENV': JSON.stringify('production')
+module.exports = webpackMerge(
+  {
+    entry: {
+      'gfpdf-backbone': './src/assets/js/legacy/gfpdf-backbone.js',
+      'gfpdf-entries': './src/assets/js/legacy/gfpdf-entries.js',
+      'gfpdf-migration': './src/assets/js/legacy/gfpdf-migration.js',
+      'gfpdf-settings': './src/assets/js/legacy/gfpdf-settings.js',
+      'app.bundle': './src/assets/js/react/gfpdf-main.js'
+    },
+    output: {
+      path: __dirname + '/dist/assets/js/',
+      filename: '[name].min.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: path.resolve(__dirname, 'src/assets/js/legacy'),
+          include: [
+            path.resolve(__dirname, 'src/assets'),
+            path.resolve(__dirname, 'tests/mocha')
+          ],
+          loader: 'babel-loader',
+          options: { babelrc: true }
+        },
+        {
+          type: 'javascript/auto',
+          test: /\.json$/,
+          loader: 'json-loader'
         }
-    }),
-    new UglifyJSPlugin({
-      parallel: true,
-      sourceMap: true,
-      uglifyOptions: {
-        output: {
-          comments: false
-        }
-      }
-    })
-  ] : []
-}
+      ]
+    },
+    externals: {
+      'jquery': 'jQuery'
+    }
+  },
+  modeConfig
+)
