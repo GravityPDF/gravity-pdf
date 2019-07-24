11666b4055fa31260005d71e76ed75810bbf7270
Jake Jackson <jake@gravitypdf.com>
2019-07-24 15:44:57 +1000

    Add Quiet Switch to the Gravity Forms CLI installation command

--- bin/setup-plugins.sh
+++ bin/setup-plugins.sh
@@ -8,7 +8,7 @@
 echo -e $(status_message "Installing and configuring Gravity Forms")
 docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm -u 33 $CLI plugin install gravityformscli --activate --force --quiet
 
-docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm -u 33 $CLI gf install --key=$GF_LICENSE --activate --force
+docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm -u 33 $CLI gf install --key=$GF_LICENSE --activate --force --quiet
 
 # Setting up Gravity PDF
 docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm -u 33 $CLI plugin activate gravity-forms-pdf-extended --quiet
