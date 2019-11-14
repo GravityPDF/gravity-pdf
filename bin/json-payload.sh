39257e760ec501575bd61386251118e7ec303afe
Jake Jackson <jake@gravitypdf.com>
2019-11-14 11:47:14 +1100

    All support for GitHub Basic Authentication to prevent Core Font JSON issue

--- bin/json-payload.sh
+++ bin/json-payload.sh
@@ -10,6 +10,13 @@ if [[ -z "$2" ]]; then
 	exit 1
 fi
 
-
+# Ensure the payload folder exists
 mkdir -p dist/payload
-curl -L $1 -o dist/payload/$2
+
+# Basic Authentication
+# $3 is a username/password combo
+if [[ -z "$3" ]]; then
+	curl -f -L $1 -o dist/payload/$2
+else
+  curl -f -u "$3" -L $1 -o dist/payload/$2
+fi
