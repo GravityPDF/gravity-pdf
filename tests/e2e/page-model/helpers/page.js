3e938fbb9cfb4d0efe38b1eb36ed09c181599d11
Jake Jackson <jake@gravitypdf.com>
2020-01-10 16:37:32 +1100

    Fix up E2E test due to Gutenberg change

--- tests/e2e/page-model/helpers/page.js
+++ tests/e2e/page-model/helpers/page.js
@@ -4,13 +4,12 @@ import { button, link } from './field'
 
 class Page {
   constructor () {
-    this.closePopupButton = Selector('button').withAttribute('aria-label', 'Disable tips')
+    this.closePopupButton = Selector('button').withAttribute('aria-label', 'Close dialog')
     this.titleField = Selector('.editor-post-title').find('textarea').withAttribute('placeholder', 'Add title')
     this.addBlockIcon = Selector('button').withAttribute('aria-label', 'Add block')
     this.searchBlock = Selector('input').withAttribute('placeholder', 'Search for a block')
     this.shortcodeLink = Selector('div').find('[class^="editor-block-types-list__item block-editor-block-types-list__item editor-block-list-item-shortcode"]')
     this.shortcodeTextarea = Selector('textarea').withAttribute('placeholder', 'Write shortcode here…')
-    this.pageHeader = Selector('.entry-header').find('h1').withText('Test page')
     this.trashLink = Selector('a').withAttribute('aria-label', 'Move “Test page” to the Trash')
   }
 
