a556ed66aa7da8669d4a07252a2bbee3a46437a3
Jake Jackson <jake@gravitypdf.com>
2020-06-16 15:59:28 +1000

    Ensure installing same version of WordPress Production / Development

--- bin/download-wordpress.sh
+++ bin/download-wordpress.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 # Download and unpack WordPress.
+curl -L http://api.wordpress.org/core/version-check/1.7/ -o ./tmp/wp-latest.json
+grep '[0-9]+\.[0-9]+(\.[0-9]+)?' ./tmp/wp-latest.json
+LATEST_VERSION=$(grep -o '"version":"[^"]*' ./tmp/wp-latest.json | sed 's/"version":"//')
+
 mkdir tmp > /dev/null 2>&1
-rm -Rf wordpress && rm -Rf tmp/wordpress-develop-master > /dev/null 2>&1
-curl -L https://wordpress.org/latest.zip -o ./tmp/latest.zip
+rm -Rf wordpress && rm -Rf tmp/wordpress-develop-$LATEST_VERSION > /dev/null 2>&1
+curl -L "https://wordpress.org/wordpress-$LATEST_VERSION.zip" -o ./tmp/latest.zip
 unzip -q ./tmp/latest.zip -d ./tmp
 mkdir -p wordpress/src
 mv ./tmp/wordpress/* wordpress/src
@@ -13,13 +17,13 @@ mkdir -p wordpress/src/wp-content/uploads
 chmod -R 767 wordpress
 
 # Grab the tools we need for WordPress' local-env.
-curl -L https://github.com/WordPress/wordpress-develop/archive/master.zip -o ./tmp/wordpress-develop.zip
+curl -L "https://github.com/WordPress/wordpress-develop/archive/$LATEST_VERSION.zip" -o ./tmp/wordpress-develop.zip
 unzip -q ./tmp/wordpress-develop.zip -d ./tmp
 mv \
-./tmp/wordpress-develop-master/tools \
-./tmp/wordpress-develop-master/tests \
-./tmp/wordpress-develop-master/.env \
-./tmp/wordpress-develop-master/docker-compose.yml \
-./tmp/wordpress-develop-master/wp-cli.yml \
-./tmp/wordpress-develop-master/*config-sample.php \
-./tmp/wordpress-develop-master/package.json wordpress
+./tmp/wordpress-develop-$LATEST_VERSION/tools \
+./tmp/wordpress-develop-$LATEST_VERSION/tests \
+./tmp/wordpress-develop-$LATEST_VERSION/.env \
+./tmp/wordpress-develop-$LATEST_VERSION/docker-compose.yml \
+./tmp/wordpress-develop-$LATEST_VERSION/wp-cli.yml \
+./tmp/wordpress-develop-$LATEST_VERSION/*config-sample.php \
+./tmp/wordpress-develop-$LATEST_VERSION/package.json wordpress
