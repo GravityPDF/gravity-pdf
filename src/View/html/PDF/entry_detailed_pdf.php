a97c2b4098f7c7a12f9641f14a795c1697d5a334
Jestoni Sumande <jestoni.hpi@gmail.com>
2020-07-16 10:38:21 +0800

    Update UI layout for pdf metabox

--- src/View/html/PDF/entry_detailed_pdf.php
+++ src/View/html/PDF/entry_detailed_pdf.php
@@ -15,28 +15,30 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 ?>
-
 <?php do_action( 'gfpdf_entry_detail_pre_pdf_title_markup', $args['pdfs'] ); ?>
 
 <?php do_action( 'gfpdf_entry_detail_post_pdf_title_markup', $args['pdfs'] ); ?>
 
+<div id="gfpdf_detailed_pdf_wrapper">
 <?php foreach ( $args['pdfs'] as $pdf ): ?>
 	<?php do_action( 'gfpdf_entry_detail_pre_container_markup', $pdf, $args['pdfs'] ); ?>
 
 	<div class="gfpdf_detailed_pdf_container">
+
 		<?php do_action( 'gfpdf_entry_detail_pre_pdf_name_markup', $pdf, $args['pdfs'] ); ?>
-		<span><?php echo esc_html( $pdf['name'] ); ?></span>
+		<div><?php echo esc_html( $pdf['name'] ); ?></div>
 		<?php do_action( 'gfpdf_entry_detail_post_pdf_name_markup', $pdf, $args['pdfs'] ); ?>
 
-		<div>
+		<div class="gfpdf_detailed_pdf_cta">
 			<?php do_action( 'gfpdf_entry_detail_pre_pdf_links_markup', $pdf, $args['pdfs'] ); ?>
-			<a href="<?php echo esc_url( $pdf['view'] ); ?>" target="_blank" class="button"><?php esc_html_e( 'View', 'gravity-forms-pdf-extended' ); ?></a>
-			<a href="<?php echo esc_url( $pdf['download'] ); ?>" class="button"><?php esc_html_e( 'Download', 'gravity-forms-pdf-extended' ); ?></a>
+			<a href="<?php echo esc_url( $pdf['view'] ); ?>"
+			   target="_blank"><?php esc_html_e( 'View', 'gravity-forms-pdf-extended' ); ?></a> |
+			<a href="<?php echo esc_url( $pdf['download'] ); ?>"><?php esc_html_e( 'Download', 'gravity-forms-pdf-extended' ); ?></a>
 			<?php do_action( 'gfpdf_entry_detail_post_pdf_links_markup', $pdf, $args['pdfs'] ); ?>
 		</div>
 	</div>
-
 	<?php do_action( 'gfpdf_entry_detail_post_container_markup', $pdf, $args['pdfs'] ); ?>
 <?php endforeach; ?>
+</div>
 
 <?php do_action( 'gfpdf_entry_detail_post_markup', $pdf, $args['pdfs'] ); ?>
