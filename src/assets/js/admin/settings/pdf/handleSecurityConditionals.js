8a9bf06938a3a2b7b852ee11f18136f09da82218
Jake Jackson <jake@gravitypdf.com>
2020-08-07 08:45:12 +1000

    Ensure Security PDF settings JS works when using translation text
    Resolves #1073
    
    # Conflicts:
    #	src/assets/js/admin/settings/pdf/handleSecurityConditionals.js

--- src/assets/js/admin/settings/pdf/handleSecurityConditionals.js
+++ src/assets/js/admin/settings/pdf/handleSecurityConditionals.js
@@ -19,6 +19,7 @@ export function handleSecurityConditionals () {
     const format = $format.filter(':checked').val()
 
     if (!$(this).is(':checked') || format !== GFPDF.standard) {
+      if ($(this).val() === 'No' || format !== 'Standard') {
       /* hide security password / privileges */
       $securityFields.hide()
     } else {
@@ -26,7 +27,7 @@ export function handleSecurityConditionals () {
       $securityFields.show()
     }
 
-    if (format !== GFPDF.standard) {
+      if (format !== 'Standard') {
       $securityQuestion.hide()
     } else {
       $securityQuestion.show()
