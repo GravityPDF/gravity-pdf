c82a824076eee6971b8c20a604f9aad9656627bb
Jake Jackson <jake@gravitypdf.com>
2020-08-12 17:19:28 +1000

    Add PDF URL to Webhook "All Fields" Request Body
    Resolves #1066

---
+++ tests/phpunit/unit-tests/Model/TestWebhook.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace GFPDF\Model;
+
+/**
+ * @package     Gravity PDF
+ * @copyright   Copyright (c) 2020, Blue Liquid Designs
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ */
+
+/**
+ * Class TestWebhook
+ *
+ * @package GFPDF\Model\Model_Form_Settings
+ *
+ * @group   model
+ * @group   form-settings
+ */
+class TestWebhook extends \WP_UnitTestCase {
+
+	/**
+	 * Test we add the PDF URLs to the Webhook request data when the request type is "all_fields"
+	 */
+	public function test_webhook_request_data_all_fields() {
+		$feed         = [ 'meta' => [ 'requestBodyType' => 'all_fields' ] ];
+		$entry        = $GLOBALS['GFPDF_Test']->entries['all-form-fields'][0];
+		$request_data = $entry;
+
+		$request_data = apply_filters( 'gform_webhooks_request_data', $request_data, $feed, $entry );
+
+		$this->assertArrayHasKey( 'gpdf_556690c67856b', $request_data );
+		$this->assertStringContainsString( 'http://example.org/?gpdf=1', $request_data['gpdf_556690c67856b'] );
+
+		$this->assertArrayHasKey( 'gpdf_fawf90c678523b', $request_data );
+		$this->assertStringContainsString( 'http://example.org/?gpdf=1', $request_data['gpdf_fawf90c678523b'] );
+	}
+
+	/**
+	 * Test that we do nothing if the request type isn't "all_fields"
+	 */
+	public function test_webhook_request_data_select_fields() {
+		$feed         = [ 'meta' => [ 'requestBodyType' => 'select_fields' ] ];
+		$entry        = $GLOBALS['GFPDF_Test']->entries['all-form-fields'][0];
+		$request_data = $entry;
+
+		$request_data = apply_filters( 'gform_webhooks_request_data', $request_data, $feed, $entry );
+
+		$this->assertSame( $entry, $request_data );
+		$this->assertArrayNotHasKey( 'gpdf_556690c67856b', $request_data );
+	}
+
+}
