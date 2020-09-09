9df499e0226d27996b524bdc6c9e9023d3e814b0
Jake Jackson <jake@gravitypdf.com>
2020-09-10 08:55:41 +1000

    Better style the actions button to match GF2.5 styles

--- src/View/html/Actions/action_buttons.php
+++ src/View/html/Actions/action_buttons.php
@@ -23,10 +23,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<input type="hidden" name="gfpdf_action_<?= $args['type']; ?>" value="<?= wp_create_nonce( 'gfpdf_action_' . $args['type'] ); ?>" />
 
 	<p>
-		<button class="button button-primary"><?= $args['button_text']; ?></button>
+		<button class="button"><?= $args['button_text']; ?></button>
 
 		<?php if ( $args['dismissal'] === 'enabled' ): ?>
-			<input class="button" type="submit" value="<?php esc_attr_e( 'Dismiss Notice', 'gravity-forms-pdf-extended' ); ?>" name="gfpdf-dismiss-notice" />
+			<input class="button primary" type="submit" value="<?php esc_attr_e( 'Dismiss Notice', 'gravity-forms-pdf-extended' ); ?>" name="gfpdf-dismiss-notice" />
 		<?php endif; ?>
 	</p>
 
