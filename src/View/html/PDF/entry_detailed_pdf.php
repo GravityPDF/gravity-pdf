95a524d69910c75589bfc222899e0b6708851594
Jestoni Sumande <jestoni.hpi@gmail.com>
2020-12-08 10:10:28 +0800

    Updates for https://app.clickup.com/t/51q3r7

--- src/View/html/PDF/entry_detailed_pdf.php
+++ src/View/html/PDF/entry_detailed_pdf.php
@@ -19,30 +19,30 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <?php do_action( 'gfpdf_entry_detail_pre_markup', $args['pdfs'] ); ?>
+<ul>
+	<?php foreach ( $args['pdfs'] as $pdf ): ?>
+		<?php do_action( 'gfpdf_entry_detail_pre_container_markup', $pdf, $args['pdfs'] ); ?>
 
-<?php foreach ( $args['pdfs'] as $pdf ): ?>
-	<?php do_action( 'gfpdf_entry_detail_pre_container_markup', $pdf, $args['pdfs'] ); ?>
+		<li class="gfpdf_detailed_pdf_container" >
 
-	<div class="gfpdf_detailed_pdf_container">
+			<?php do_action( 'gfpdf_entry_detail_pre_pdf_name_markup', $pdf, $args['pdfs'] ); ?>
 
-		<?php do_action( 'gfpdf_entry_detail_pre_pdf_name_markup', $pdf, $args['pdfs'] ); ?>
+			<div><?= esc_html( $pdf['name'] ); ?></div>
 
-		<div><?= esc_html( $pdf['name'] ); ?></div>
+			<?php do_action( 'gfpdf_entry_detail_post_pdf_name_markup', $pdf, $args['pdfs'] ); ?>
 
-		<?php do_action( 'gfpdf_entry_detail_post_pdf_name_markup', $pdf, $args['pdfs'] ); ?>
+			<div class="gfpdf_detailed_pdf_cta" aria-label="View or Download <?= $pdf['settings']['filename'] ?>.pdf ">
+				<?php do_action( 'gfpdf_entry_detail_pre_pdf_links_markup', $pdf, $args['pdfs'] ); ?>
 
-		<div class="gfpdf_detailed_pdf_cta">
-			<?php do_action( 'gfpdf_entry_detail_pre_pdf_links_markup', $pdf, $args['pdfs'] ); ?>
+				<a href="<?= esc_url( $pdf['view'] ) ?>" target="_blank"><?= esc_html__( 'View', 'gravity-forms-pdf-extended' ) ?></a> |
+				<a href="<?= esc_url( $pdf['download'] ) ?>"><?= esc_html__( 'Download', 'gravity-forms-pdf-extended' ); ?></a>
 
-			<a href="<?= esc_url( $pdf['view'] ) ?>" target="_blank"><?= esc_html__( 'View', 'gravity-forms-pdf-extended' ) ?></a> |
-			<a href="<?= esc_url( $pdf['download'] ) ?>"><?= esc_html__( 'Download', 'gravity-forms-pdf-extended' ); ?></a>
+				<?php do_action( 'gfpdf_entry_detail_post_pdf_links_markup', $pdf, $args['pdfs'] ); ?>
+			</div>
+		</li>
 
-			<?php do_action( 'gfpdf_entry_detail_post_pdf_links_markup', $pdf, $args['pdfs'] ); ?>
-		</div>
-	</div>
+		<?php do_action( 'gfpdf_entry_detail_post_container_markup', $pdf, $args['pdfs'] ); ?>
 
-	<?php do_action( 'gfpdf_entry_detail_post_container_markup', $pdf, $args['pdfs'] ); ?>
-
-<?php endforeach; ?>
-
+	<?php endforeach; ?>
+</ul>
 <?php do_action( 'gfpdf_entry_detail_post_markup', $args['pdfs'] ); ?>
