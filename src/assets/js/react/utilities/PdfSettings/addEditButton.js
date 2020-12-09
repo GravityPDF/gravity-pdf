924af0975026d76c13085f6f7d71f738be99be29
Debbie <debbieellis7@gmail.com>
2020-12-09 10:36:51 +0800

    Add/Update button below PDF settings container

---
+++ src/assets/js/react/utilities/PdfSettings/addEditButton.js
@@ -0,0 +1,38 @@
+export default function addEditButton (pdfSettingFieldSets, form) {
+  const items = Array.from(pdfSettingFieldSets)
+  /* Remove last element of the array */
+  items.pop()
+
+  items.map((fieldset, index) => {
+    const collapsibleToggleIcon = fieldset.querySelector('.gform-settings-panel__collapsible-toggle-checkbox')
+
+    window.addEventListener('load', function () {
+      insertAfter(fieldset, form, index, 'firstLoad')
+    })
+
+    collapsibleToggleIcon.addEventListener('click', function () {
+      insertAfter(fieldset, form, index)
+    })
+  })
+}
+
+export function insertAfter (fieldset, form, index, firstLoad) {
+  const wrapperClass = 'submit-container-' + index
+
+  if (!fieldset.classList.contains('gform-settings-panel--collapsed')) {
+    const submitButton = form.querySelector('#submit')
+    const submitButtonClone = submitButton.cloneNode(true)
+    const wrapper = document.createElement('div')
+    wrapper.setAttribute('class', wrapperClass)
+    wrapper.innerHTML = submitButtonClone.outerHTML
+
+    return fieldset.parentNode.insertBefore(wrapper, fieldset.nextSibling)
+  }
+
+  if (firstLoad) {
+    return
+  }
+
+  /* Remove button when fieldset collapsed */
+  document.querySelector(`.${wrapperClass}`).remove()
+}
