ee7577496e48dbd33908f8ec5da823d86b0b59a6
Jake Jackson <jake@gravitypdf.com>
2021-03-01 14:04:22 +1100

    Write to log right after initialisation
    Because it's in a try/catch block, if the write fails, the filesystem log will be ignored and all logs will be discarded.
    
    This should resolve #1116

--- src/Helper/Helper_Logger.php
+++ src/Helper/Helper_Logger.php
@@ -119,6 +119,8 @@ class Helper_Logger {
 				$this->log->pushProcessor( new IntrospectionProcessor );
 				$this->log->pushProcessor( new MemoryPeakUsageProcessor );
 
+				$this->log->notice( 'Log initialized' );
+
 				return;
 			}
 		} catch ( Exception $e ) {
