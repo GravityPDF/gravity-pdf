378ef4e57e3a7017db86958d5c047e36d4007393
Debbie <debbieellis7@gmail.com>
2021-03-20 12:39:12 +0800

    Adjust uninstall gravity pdf e2e check

--- tests/e2e/tabs/tools/uninstall-gravity-pdf-field.test.js
+++ tests/e2e/tabs/tools/uninstall-gravity-pdf-field.test.js
@@ -1,4 +1,3 @@
-import { fieldHeaderTitle, fieldDescription } from '../../utilities/page-model/helpers/field'
 import Tools from '../../utilities/page-model/tabs/tools'
 
 const run = new Tools()
@@ -7,12 +6,11 @@ fixture`Tools tab - Uninstall Gravity PDF field test`
 
 test('should display \'Uninstall Gravity PDF\' field', async t => {
   // Actions
-  await run.navigateSettingsTab('gf_settings&subview=PDF&tab=tools#')
-  await t.click(run.uninstallGravityPdfCollapsiblePanel)
+  await run.navigateSettingsTab('gf_settings&subview=uninstall')
 
   // Assertions
   await t
-    .expect(fieldHeaderTitle('Uninstall Gravity PDF').exists).ok()
-    .expect(fieldDescription('This operation deletes ALL Gravity PDF settings and deactivates the plugin. If you continue, all settings, configuration, custom templates and fonts will be removed.', 'p').exists).ok()
+    .expect(run.uninstallPanelTitle.exists).ok()
+    .expect(run.uninstallPanelDescription.exists).ok()
     .expect(run.uninstallGravityPdfButton.exists).ok()
 })
