d3712354c21ad14cec09ab98978de5c2a2d93c27
Debbie <debbieellis7@gmail.com>
2021-03-22 07:30:04 +0800

    auto select shortcode field

---
+++ src/assets/js/react/utilities/PdfList/autoSelectShortcode.js
@@ -0,0 +1,16 @@
+export default function autoSelectShortcode (gfPdfListForm) {
+  const shortcodeFields = Array.from(gfPdfListForm.querySelectorAll('td.column-shortcode'))
+
+  if (shortcodeFields.length > 0) {
+    shortcodeFields.map(field => {
+      addEventListener(field)
+    })
+  }
+}
+
+export function addEventListener (field) {
+  field.addEventListener('click', () => {
+    document.execCommand('selectall', null, false)
+    document.execCommand('copy')
+  })
+}
