a277e9f20f2100f0b91a7b376e032b5dacb30883
Jestoni Sumande <jestoni.sumande@gmail.com>
2021-06-01 13:11:49 +0800

    Don’t display Radio/Select Field if empty value
    Change the behaviour of Radio and Select fields so that we only look at the field “value” when determining if it should get displayed in the PDF.
    
    This change doesn’t effect the $form_data array, and only applies to Core / Universal templates.
    
    A filter has been added “gfpdf_field_is_empty_value_instead_of_label” for backwards compatibility, should users want the old functionality.
    
    Resolves #1204

---
+++ tests/phpunit/unit-tests/Helper/Fields/Test_Field_Select.php
@@ -0,0 +1,73 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace GFPDF\Helper\Fields;
+
+use WP_UnitTestCase;
+
+/**
+ * @package     Gravity PDF
+ * @copyright   Copyright (c) 2021, Blue Liquid Designs
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ */
+
+/**
+ * Class Test_FlushCache
+ *
+ * @package GFPDF\Helper\Fonts
+ *
+ * @group   helper
+ * @group   fields
+ */
+class Test_Field_Select extends WP_UnitTestCase {
+
+	public $form;
+
+	public $gf_field;
+
+	public $pdf_field;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->form = $GLOBALS['GFPDF_Test']->form['all-form-fields'];
+
+		foreach ( $this->form['fields'] as $field ) {
+			if ( $field->type === 'select' ) {
+				$this->gf_field = new \GF_Field_Select( $field );
+				break;
+			}
+		}
+
+		$entry = [
+			'form_id' => $this->form['id'],
+		];
+
+		$this->pdf_field = new Field_Select( $this->gf_field, $entry, \GPDFAPI::get_form_class(), \GPDFAPI::get_misc_class() );
+	}
+
+	public function test_is_empty() {
+		$this->assertTrue( $this->pdf_field->is_empty() );
+
+		add_filter( 'gfpdf_field_is_empty_value_instead_of_label', '__return_false' );
+
+		$this->assertFalse( $this->pdf_field->is_empty() );
+
+		remove_filter( 'gfpdf_field_is_empty_value_instead_of_label', '__return_false' );
+	}
+
+	public function test_value_with_empty_value() {
+		$value = $this->pdf_field->value();
+
+		$this->assertEmpty( $value['value'] );
+		$this->assertNotEmpty( $value['label'] );
+	}
+
+	public function test_form_data_with_empty_value() {
+		$form_data = $this->pdf_field->form_data();
+
+		$this->assertSame( '', $form_data['field'][ $this->gf_field->id ] );
+		$this->assertSame( 'Option 4', $form_data['field'][ $this->gf_field->id . '_name' ] );
+	}
+}
