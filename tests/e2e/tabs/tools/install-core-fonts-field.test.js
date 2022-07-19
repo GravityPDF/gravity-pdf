6463dfacead61d4df8cda9aaf1d32b5aefaa1c45
Jake Jackson <jake@gravitypdf.com>
2022-07-19 10:53:36 +1000

    Revert

--- tests/e2e/tabs/tools/install-core-fonts-field.test.js
+++ tests/e2e/tabs/tools/install-core-fonts-field.test.js
@@ -4,8 +4,12 @@ import { fieldHeaderTitle } from '../../utilities/page-model/helpers/field'
 import Tools from '../../utilities/page-model/tabs/tools'
 
 const run = new Tools()
+const mockSuccess = RequestMock()
+  .onRequestTo(`${baseURL}/wp-admin/admin-ajax.php`)
+  .respond({}, 200, { 'access-Control-Allow-Origin': '*' })
 
-fixture`Tools tab - Core Fonts`
+fixture`Tools tab - Install core fonts field test`
+  .requestHooks(mockSuccess)
 
 test('should display \'Install Core Fonts\' field', async t => {
   // Actions
@@ -17,28 +21,26 @@ test('should display \'Install Core Fonts\' field', async t => {
     .expect(run.downloadCoreFontsButton.exists).ok()
 })
 
-const mockSuccess = RequestMock()
-  .onRequestTo(`${baseURL}/wp-admin/admin-ajax.php`)
-  .respond({}, 200, { 'access-Control-Allow-Origin': '*' })
+test('should return download core fonts successful response ', async t => {
+  // Actions
+  await run.navigateSettingsTab('gf_settings&subview=PDF&tab=tools#')
+  await t.click(run.downloadCoreFontsButton)
 
-test
-  .requestHooks(mockSuccess)('should return download core fonts successful response ', async t => {
-    // Actions
-    await run.navigateSettingsTab('gf_settings&subview=PDF&tab=tools#')
-    await t.click(run.downloadCoreFontsButton)
-
-    // Assertions
-    await t
-      .expect(run.pendingResult.exists).ok()
-      .expect(run.downloadSuccess.exists).ok()
-      .expect(run.allSuccessfullyIntalled.exists).ok()
-  })
+  // Assertions
+  await t
+    .expect(run.pendingResult.exists).ok()
+    .expect(run.downloadSuccess.exists).ok()
+    .expect(run.allSuccessfullyIntalled.exists).ok()
+})
 
 const mockFailure = RequestMock()
   .onRequestTo(`${baseURL}/wp-admin/admin-ajax.php`)
   .respond({}, 500, { 'access-Control-Allow-Origin': '*' })
 
-test.requestHooks(mockFailure)('should return download core fonts error/failed response', async t => {
+fixture``
+  .requestHooks(mockFailure)
+
+test('should return download core fonts error/failed response', async t => {
   // Actions
   await run.navigateSettingsTab('gf_settings&subview=PDF&tab=tools#')
   await t.click(run.downloadCoreFontsButton)
