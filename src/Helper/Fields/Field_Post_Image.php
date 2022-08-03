b4a255c0fcdcfb9d8f1413a070c1b2c2a2220c4b
Jake Jackson <jake@gravitypdf.com>
2022-08-03 15:46:00 +1000

    Fix URL Escape issue when using Image Path
    Resolves escaping issue when using a Windows-based local path in images.

--- src/Helper/Fields/Field_Post_Image.php
+++ src/Helper/Fields/Field_Post_Image.php
@@ -65,7 +65,8 @@ class Field_Post_Image extends Helper_Abstract_Fields {
 		$html = '';
 		if ( ! empty( $value['url'] ) ) {
 			$html  = '<a href="' . esc_url( $value['url'] ) . '" target="_blank">';
-			$html .= '<img width="150" src="' . esc_url( $value['url'] ) . '" />';
+			$html .= '<img width="150" src="' . ( $value['path'] !== $value['url'] ? esc_attr( $value['path'] ) : esc_url( $value['url'] ) ) . '" />';
+			$html .= '</a>';
 		}
 
 		/* Include title / caption / description if needed */
@@ -80,8 +81,6 @@ class Field_Post_Image extends Helper_Abstract_Fields {
 		if ( ! empty( $value['description'] ) ) {
 			$html .= '<div class="gfpdf-post-image-description">' . esc_html( $value['description'] ) . '</div>';
 		}
-
-		$html .= '</a>';
 
 		return parent::html( $html );
 	}
@@ -131,13 +130,13 @@ class Field_Post_Image extends Helper_Abstract_Fields {
 			$value = explode( '|:|', $this->get_value() );
 
 			$img['url']         = ( isset( $value[0] ) ) ? esc_url( $value[0] ) : '';
-			$img['path']        = ( isset( $value[0] ) ) ? esc_url( $value[0] ) : '';
+			$img['path']        = $img['url'];
 			$img['title']       = ( isset( $value[1] ) ) ? esc_html( $value[1] ) : '';
 			$img['caption']     = ( isset( $value[2] ) ) ? esc_html( $value[2] ) : '';
 			$img['description'] = ( isset( $value[3] ) ) ? esc_html( $value[3] ) : '';
 
 			$path = ( isset( $value[0] ) ) ? $this->misc->convert_url_to_path( $value[0] ) : '';
-			if ( $path !== $img['url'] ) {
+			if ( $path !== false && $path !== $img['url'] ) {
 				$img['path'] = $path;
 			}
 		}
---
+++ tests/phpunit/unit-tests/Helper/Fields/Test_Field_Signature.php
@@ -0,0 +1,117 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace GFPDF\Helper\Fields;
+
+use WP_UnitTestCase;
+
+/**
+ * @package     Gravity PDF
+ * @copyright   Copyright (c) 2022, Blue Liquid Designs
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ */
+
+/**
+ * @group   helper
+ * @group   fields
+ */
+class Test_Field_Signature extends WP_UnitTestCase {
+
+	/**
+	 * @var array
+	 */
+	public $form;
+
+	/**
+	 * @var \GF_Field_Signature
+	 */
+	public $gf_field;
+
+	public function set_up() {
+		parent::set_up();
+
+		$this->form = $GLOBALS['GFPDF_Test']->form['all-form-fields'];
+
+		foreach ( $this->form['fields'] as $field ) {
+			if ( $field->type === 'signature' ) {
+				$this->gf_field = $field;
+				break;
+			}
+		}
+	}
+
+	public function test_html_with_windows_drive_path() {
+		$entry = $GLOBALS['GFPDF_Test']->entries['all-form-fields'][0];
+		$field = new class( $this->gf_field, $entry, \GPDFAPI::get_form_class(), \GPDFAPI::get_misc_class() ) extends Field_Signature {
+			public function value() {
+				return [
+					'img' => '<img width="150" src="c:\My Documents\Images\image.jpg" />',
+				];
+			}
+
+			public function is_empty() {
+				return false;
+			}
+		};
+
+		$html = str_replace( "\n", '', $field->html() );
+
+		$this->assertStringContainsString( '<img width="150" src="c:\My Documents\Images\image.jpg" />', $html );
+	}
+
+	public function test_html_with_windows_unc_path() {
+		$entry = $GLOBALS['GFPDF_Test']->entries['all-form-fields'][0];
+		$field = new class( $this->gf_field, $entry, \GPDFAPI::get_form_class(), \GPDFAPI::get_misc_class() ) extends Field_Signature {
+			public function value() {
+				return [
+					'img' => '<img width="150" src="\\My Documents\Images\image.jpg" />',
+				];
+			}
+
+			public function is_empty() {
+				return false;
+			}
+		};
+
+		$html = str_replace( "\n", '', $field->html() );
+
+		$this->assertStringContainsString( '<img width="150" src="\\My Documents\Images\image.jpg" />', $html );
+
+		/* UNC path pointed to named drive on the network */
+		$field = new class( $this->gf_field, $entry, \GPDFAPI::get_form_class(), \GPDFAPI::get_misc_class() ) extends Field_Signature {
+			public function value() {
+				return [
+					'img' => '<img width="150" src="\\system07\C$\My Documents\Images\image.jpg" />',
+				];
+			}
+
+			public function is_empty() {
+				return false;
+			}
+		};
+
+		$html = str_replace( "\n", '', $field->html() );
+
+		$this->assertStringContainsString( '<img width="150" src="\\system07\C$\My Documents\Images\image.jpg" />', $html );
+	}
+
+	public function test_html_with_linux_path() {
+		$entry = $GLOBALS['GFPDF_Test']->entries['all-form-fields'][0];
+		$field = new class( $this->gf_field, $entry, \GPDFAPI::get_form_class(), \GPDFAPI::get_misc_class() ) extends Field_Signature {
+			public function value() {
+				return [
+					'img' => '<img width="150" src="/var/www/html/image.jpg" />',
+				];
+			}
+
+			public function is_empty() {
+				return false;
+			}
+		};
+
+		$html = str_replace( "\n", '', $field->html() );
+
+		$this->assertStringContainsString( '<img width="150" src="/var/www/html/image.jpg" />', $html );
+	}
+}
