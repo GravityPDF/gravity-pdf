fb36ccf0abdd1c4ff7ac2b8a4b3fe45002e6206f
debbieellis7 <1debbieellis@gmail.com>
2022-12-05 11:59:55 +0800

    Fix unwanted &nbsp added into page title

--- tests/e2e/utilities/page-model/helpers/page.js
+++ tests/e2e/utilities/page-model/helpers/page.js
@@ -4,14 +4,14 @@ import { link } from './field'
 
 class Page {
   constructor () {
-    this.testPageLink = link('#the-list', 'Test page')
+    this.testPageLink = link('#the-list', 'Test-page')
     this.closePopupButton = Selector('button').withAttribute('aria-label', 'Close dialog')
     this.titleField = Selector('.editor-post-title__input')
     this.addBlockIcon = Selector('button').withAttribute('aria-label', 'Add block')
     this.searchBlock = Selector('.block-editor-inserter__search').find('input').withAttribute('type', 'search')
     this.shortcodeLink = Selector('button.editor-block-list-item-shortcode')
     this.shortcodeTextarea = Selector('textarea').withAttribute('placeholder', 'Write shortcode here…')
-    this.trashLink = Selector('a').withAttribute('aria-label', 'Move “Test page” to the Trash')
+    this.trashLink = Selector('a').withAttribute('aria-label', 'Move “Test-page” to the Trash')
     this.publishButton = Selector('.edit-post-header__settings').find('button').withText('Publish')
     this.confirmPublishButton = Selector('.editor-post-publish-panel__header').find('button').withText('Publish')
     this.updateButton = Selector('.edit-post-header__settings').find('button').withText('Update')
@@ -25,10 +25,12 @@ class Page {
   }
 
   async addNewPage () {
+    await t.click(link('.wrap', 'Add New'))
+
+    if (await this.closePopupButton.exists) await t.click(this.closePopupButton)
+
     await t
-      .click(link('.wrap', 'Add New'))
-      .click(this.closePopupButton)
-      .typeText(this.titleField, 'Test page')
+      .typeText(this.titleField, 'Test-page', { paste: true })
       .click(this.publishButton)
       .click(this.confirmPublishButton)
   }
@@ -36,7 +38,7 @@ class Page {
   async deleteTestPage () {
     await t
       .navigateTo(`${baseURL}/wp-admin/edit.php?post_type=page`)
-      .hover(link('#the-list', 'Test page'))
+      .hover(link('#the-list', 'Test-page'))
       .click(this.trashLink)
   }
 }
