73e0bcc35cf4ae4853ac24b6f900da96d7654f7b
Jake Jackson <jake@gravitypdf.com>
2023-08-25 11:22:09 +1000

    no message (+1 squashed commit) Squashed commits: [22ada1c8] Fix autoloading errors

--- .php-scoper/mpdf.php
+++ .php-scoper/mpdf.php
@@ -27,7 +27,6 @@ return [
 		Finder::create()->files()->in( $path . 'vendor/mpdf/qrcode/' )->exclude( 'tests' )->name( [ '*.php', 'LICENSE', '*.dat' ] ),
 		Finder::create()->files()->in( $path . 'vendor/mpdf/psr-log-aware-trait' )->name( [ '*.php' ] ),
 		Finder::create()->files()->in( $path . 'vendor/psr/log' )->name( [ '*.php', 'LICENSE' ] ),
-		Finder::create()->files()->in( $path . 'vendor/psr/http-message' )->name( [ '*.php', 'LICENSE' ] ),
 		Finder::create()->files()->in( $path . 'vendor/setasign/fpdi' )->name( [ '*.php', 'LICENSE.txt' ] ),
 		Finder::create()->files()->in( $path . 'vendor/myclabs/deep-copy' )->name( [ '*.php', 'LICENSE' ] ),
 	],
@@ -75,5 +74,9 @@ return [
 
 			return $content;
 		},
+	],
+
+	'whitelist' => [
+		'Psr\Http\*',
 	],
 ];
