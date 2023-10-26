7bf02c4291f632129ccb48176abbde294289bd53
Jake Jackson <jake@gravitypdf.com>
2023-10-26 15:17:06 +1100

    Revert trash link selector

--- tests/e2e/utilities/page-model/helpers/page.js
+++ tests/e2e/utilities/page-model/helpers/page.js
@@ -12,7 +12,7 @@ class Page {
     this.searchBlock = Selector('.block-editor-inserter__search').find('input').withAttribute('type', 'search')
     this.shortcodeLink = Selector('button.editor-block-list-item-shortcode')
     this.shortcodeTextarea = Selector('textarea').withAttribute('placeholder', 'Write shortcode here…')
-    this.trashLink = Selector('a').withText('Trash')
+    this.trashLink = Selector('a').withAttribute('aria-label', 'Move “Test-page” to the Trash')
     this.publishButton = Selector('.edit-post-header__settings').find('button').withText('Publish')
     this.confirmPublishButton = Selector('.editor-post-publish-panel__header').find('button').withText('Publish')
     this.updateButton = Selector('.edit-post-header__settings').find('button').withText('Update')
