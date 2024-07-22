9b0448a4347cdc099b745a42eeb32d3c0c84bbed
Jake Jackson <jake@gravitypdf.com>
2024-07-22 11:21:55 +1000

    Version bump

--- pdf.php
+++ pdf.php
@@ -1,8 +1,8 @@
 <?php
 /*
 Plugin Name: Gravity PDF
-Version: 6.11.0
-Description: Automatically generate highly-customisable PDF documents using Gravity Forms.
+Version: 6.11.1
+Description: Automatically generate highly customizable PDF documents using Gravity Forms.
 Author: Blue Liquid Designs
 Author URI: https://blueliquiddesigns.com.au
 Plugin URI: https://gravitypdf.com
@@ -28,7 +28,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /*
  * Set base constants we'll use throughout the plugin
  */
-define( 'PDF_EXTENDED_VERSION', '6.11.0' ); /* the current plugin version */
+define( 'PDF_EXTENDED_VERSION', '6.11.1' ); /* the current plugin version */
 define( 'PDF_PLUGIN_DIR', plugin_dir_path( __FILE__ ) ); /* plugin directory path */
 define( 'PDF_PLUGIN_URL', plugin_dir_url( __FILE__ ) ); /* plugin directory url */
 define( 'PDF_PLUGIN_BASENAME', plugin_basename( __FILE__ ) ); /* the plugin basename */
