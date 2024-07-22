ffa03a7f8dddc309130c636c9414660fb3a8e230
Jake Jackson <jake@gravitypdf.com>
2024-07-22 13:23:12 +1000

    Add docblocks for the debug class

--- src/Statics/Debug.php
+++ src/Statics/Debug.php
@@ -15,18 +15,35 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /**
  * @since 6.12.0
- * @TODO
  */
 class Debug {
+
+	/**
+	 * Checks if the WP site is in debug mode
+	 *
+	 * Currently, debug mode is considered on if the PDF Debug Mode setting is explicitly enabled
+	 * or the WP environment is not set to production
+	 *
+	 * @return bool
+	 *
+	 * @since 6.12
+	 */
 	public static function is_enabled(): bool {
 		$options = \GPDFAPI::get_options_class();
 
 		$pdf_debug_mode            = $options->get_option( 'debug_mode', 'No' ) === 'Yes';
-		$wp_production_environment = function_exists( 'wp_get_environment_type' ) && wp_get_environment_type() !== 'production';
+		$wp_production_environment = ! function_exists( 'wp_get_environment_type' ) || wp_get_environment_type() === 'production';
 
-		return $pdf_debug_mode && ! $wp_production_environment;
+		return $pdf_debug_mode || ! $wp_production_environment;
 	}
 
+	/**
+	 * Check the logged-in user has a specific capability which can view the log info
+	 *
+	 * @return bool
+	 *
+	 * @since 6.12
+	 */
 	public static function can_view(): bool {
 		$gform = \GPDFAPI::get_form_class();
 
@@ -34,6 +51,13 @@ class Debug {
 
 	}
 
+	/**
+	 * Verify debug mode is enabled and the user can view the log info
+	 *
+	 * @return bool
+	 *
+	 * @since 6.12
+	 */
 	public static function is_enabled_and_can_view(): bool {
 		return static::is_enabled() && static::can_view();
 	}
