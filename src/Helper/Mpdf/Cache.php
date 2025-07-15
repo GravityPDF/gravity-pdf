a1caf1f7b74e084fb699f133248575c4abc5623d
Jake Jackson <jake@gravitypdf.com>
2025-07-15 12:07:07 +1000

    Mpdf Improvements
    - Replace Cache class with version that better accounts for directory permissions of the parent folder
    - Remove creation of unnecessary ttfontdata folder
    - Move override files/classes to new folder for easier development

---
+++ src/Helper/Mpdf/Cache.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace GFPDF\Helper\Mpdf;
+
+use GFPDF_Vendor\Mpdf\Cache as MpdfCache;
+
+/**
+ * @since 6.13.0
+ */
+class Cache extends MpdfCache {
+
+	/**
+	 * Override to ensure folder permissions are being set based on the parent directory
+	 *
+	 * @param $basePath
+	 *
+	 * @return bool
+	 *
+	 * @since 6.13.0
+	 */
+	protected function createDirectory( $basePath ) {
+		if ( ! wp_mkdir_p( $basePath ) ) {
+			return false;
+		}
+
+		return true;
+	}
+}
