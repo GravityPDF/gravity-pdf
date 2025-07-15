ea92af7beb3bf87ec0e351394b9543c6b3cda2d4
Jake Jackson <jake@gravitypdf.com>
2025-07-15 12:07:07 +1000

    Mpdf Improvements
    - Replace Cache class with version that better accounts for directory permissions of the parent folder
    - Remove creation of unnecessary ttfontdata folder
    - Move override files/classes to new folder for easier development

---
+++ tests/phpunit/unit-tests/Helper/Mpdf/Test_Cache.php
@@ -0,0 +1,56 @@
+
+<?php
+
+declare( strict_types=1 );
+
+namespace GFPDF\Helper\Mpdf;
+
+use WP_UnitTestCase;
+
+/**
+ * @package     Gravity PDF
+ * @copyright   Copyright (c) 2025, Blue Liquid Designs
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ */
+
+/**
+ * @group   helper
+ */
+class Test_Cache extends WP_UnitTestCase {
+
+	/**
+	 * Verify the cache directory inherits the parent directory permissions
+	 *
+	 * @dataProvider provider_createDirectory
+	 */
+	public function test_createDirectory( $permission ) {
+		clearstatcache();
+
+		$basepath     = sys_get_temp_dir() . '/mpdf-cache/';
+		$tmp_basepath = $basepath . 'tmp/';
+
+		/* ensure we have a clean slate */
+		@rmdir( $tmp_basepath );
+		@rmdir( $basepath );
+
+		/* Create the base directory with the correct permissions */
+		mkdir( $basepath );
+		chmod( $basepath, $permission );
+
+		new Cache( $tmp_basepath );
+
+		$base_permission = substr( decoct( fileperms( $basepath ) ), -3 );
+		$tmp_permission = substr( decoct( fileperms( $tmp_basepath ) ), -3 );
+
+		$this->assertSame( decoct( $permission ), $base_permission );
+		$this->assertSame( decoct( $permission ), $tmp_permission );
+	}
+
+	public function provider_createDirectory() {
+		return [
+			[ 0755 ],
+			[ 0775 ],
+			[ 0777 ],
+		];
+	}
+}
