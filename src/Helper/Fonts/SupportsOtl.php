a5caf495846e7c1413f35689d4952cf2cb2408f0
Jake Jackson <jake@gravitypdf.com>
2025-07-17 16:19:42 +1000

    Use new Mpdf cache class when validating custom fonts

--- src/Helper/Fonts/SupportsOtl.php
+++ src/Helper/Fonts/SupportsOtl.php
@@ -4,7 +4,7 @@ declare( strict_types=1 );
 
 namespace GFPDF\Helper\Fonts;
 
-use GFPDF_Vendor\Mpdf\Cache;
+use GFPDF\Helper\Mpdf\Cache;
 use GFPDF_Vendor\Mpdf\Fonts\FontCache;
 use GFPDF_Vendor\Mpdf\MpdfException;
 use GFPDF_Vendor\Mpdf\TTFontFile;
@@ -46,10 +46,12 @@ class SupportsOtl {
 	 */
 	public function supports_otl( string $file ): bool {
 		try {
-			$ttf = new TTFontFile( new FontCache( new Cache( get_temp_dir() . 'mpdf' ) ), apply_filters( 'gpdf_mpdf_font_descriptor', 'win' ) );
+			$data = \GPDFAPI::get_data_class();
+
+			$ttf = new TTFontFile( new FontCache( new Cache( $data->mpdf_tmp_location . '/mpdf' ) ), apply_filters( 'gpdf_mpdf_font_descriptor', 'win' ) );
 			$ttf->getMetrics( $this->font_directory_path . $file, (string) time(), 0, false, false, 0xFF );
 
-			return strlen( $ttf->familyName ) > 0;
+			return ! empty( $ttf->familyName );
 		} catch ( MpdfException $e ) {
 
 		}
