0f3e4593abe046fc00caa61f98e38430d162ff1d
Jake Jackson <jake@gravitypdf.com>
2025-08-25 14:18:39 +1000

    Test for pretty permalinks in merge tag attribute

--- tests/playwright/download-mergetag/attributes.spec.ts
+++ tests/playwright/download-mergetag/attributes.spec.ts
@@ -4,6 +4,58 @@ import type { Page } from '@playwright/test';
 import Pdf from '@self:playwright/utils/gravitypdf';
 
 test.describe('Mergetag attributes', () => {
+  let pdf = null;
+  let form = null;
+  let pdfId = null;
+
+  test.beforeEach(async ({
+                           requestUtils,
+                           page,
+                           admin,
+                         }: {
+    requestUtils: RequestUtils;
+    page: Page;
+    admin: Admin;
+  }) => {
+    pdf = new Pdf(requestUtils, admin, page);
+
+    // setup form and PDF
+    form = await pdf.createForm('Mergetag Attributes');
+    await pdf.navigateToFormPreview(form.id);
+    await pdf.saveForm();
+    pdfId = await pdf.createPdf(form.id, 'Mergetag');
+
+    // setup default confirmation
+    await pdf.navigateToFormConfirmation(form.id);
+
+    // Clear confirmation message and use the mergetag selector
+    const content = `
+        Download: {Mergetag:pdf:${pdfId}:download}
+        Print: {Mergetag:pdf:${pdfId}:print}
+        Signed: {Mergetag:pdf:${pdfId}:signed}
+        Signed 5: {Mergetag:pdf:${pdfId}:signed, 5 minutes}
+        Print Download: {Mergetag:pdf:${pdfId}:download:print}
+        Print Signed: {Mergetag:pdf:${pdfId}:signed:print}
+        Download Signed: {Mergetag:pdf:${pdfId}:download:signed}
+        `;
+    await pdf.setRichTextContent('#gform_setting_message', content);
+    await pdf.saveForm();
+  })
+
+  test.afterEach(async({
+                         requestUtils,
+                         page,
+                         admin,
+                       }: {
+    requestUtils: RequestUtils;
+    page: Page;
+    admin: Admin;
+  }) => {
+    await admin.visitAdminPage('options-permalink.php');
+    await page.getByRole('radio', { name: 'Plain' }).click();
+    await page.getByRole('button', { name: 'Save Changes' }).click();
+  })
+
 	test('Plain Permalinks', async ({
 		requestUtils,
 		page,
@@ -13,30 +65,6 @@ test.describe('Mergetag attributes', () => {
 		page: Page;
 		admin: Admin;
 	}) => {
-		const pdf = new Pdf(requestUtils, admin, page);
-
-		// setup form and PDF
-		const form = await pdf.createForm('Mergetag Attributes');
-		await pdf.navigateToFormPreview(form.id);
-		await pdf.saveForm();
-		const pdfId = await pdf.createPdf(form.id, 'Mergetag');
-
-		// setup default confirmation
-		await pdf.navigateToFormConfirmation(form.id);
-
-		// Clear confirmation message and use the mergetag selector
-		const content = `
-        Download: {Mergetag:pdf:${pdfId}:download}
-        Print: {Mergetag:pdf:${pdfId}:print}
-        Signed: {Mergetag:pdf:${pdfId}:signed}
-        Signed 5: {Mergetag:pdf:${pdfId}:signed, 5 minutes}
-        Print Download: {Mergetag:pdf:${pdfId}:download:print}
-        Print Signed: {Mergetag:pdf:${pdfId}:signed:print}
-        Download Signed: {Mergetag:pdf:${pdfId}:download:signed}
-        `;
-		await pdf.setRichTextContent('#gform_setting_message', content);
-		await pdf.saveForm();
-
 		// preview and submit form
 		await pdf.navigateToFormPreview(form.id);
 		await pdf.saveForm();
@@ -79,4 +107,60 @@ test.describe('Mergetag attributes', () => {
 			)
 		);
 	});
+
+  test('Pretty Permalinks', async ({
+                                    requestUtils,
+                                    page,
+                                    admin,
+                                  }: {
+    requestUtils: RequestUtils;
+    page: Page;
+    admin: Admin;
+  }) => {
+    await admin.visitAdminPage('options-permalink.php')
+    await page.getByRole('radio', { name: 'Post name' }).click()
+    await page.getByRole('button', { name: 'Save Changes' }).click()
+
+    // preview and submit form
+    await pdf.navigateToFormPreview(form.id);
+    await pdf.saveForm();
+
+    // verify the results
+    const confirmation = await page.locator('#preview_form_container');
+    await expect(confirmation).toContainText(
+      new RegExp(
+        `Download: http:\/\/(.+?)\/pdf\/${pdfId}\/([0-9]+)\/download\/`
+      )
+    );
+    await expect(confirmation).toContainText(
+      new RegExp(
+        `Print: http:\/\/(.+?)\/pdf\/${pdfId}\/([0-9]+)\/\\?print=1`
+      )
+    );
+    await expect(confirmation).toContainText(
+      new RegExp(
+        `Signed: http:\/\/(.+?)\/pdf\/${pdfId}\/([0-9]+)\/\\?expires=([0-9]+)&signature=(.+?)`
+      )
+    );
+    await expect(confirmation).toContainText(
+      new RegExp(
+        `Signed 5: http:\/\/(.+?)\/pdf\/${pdfId}\/([0-9]+)\/\\?expires=([0-9]+)&signature=(.+?)`
+      )
+    );
+    await expect(confirmation).toContainText(
+      new RegExp(
+        `Print Download: http:\/\/(.+?)\/pdf\/${pdfId}\/([0-9]+)\/download\/\\?print=1`
+      )
+    );
+    await expect(confirmation).toContainText(
+      new RegExp(
+        `Print Signed: http:\/\/(.+?)\/pdf\/${pdfId}\/([0-9]+)\/\\?print=1&expires=([0-9]+)&signature=(.+?)`
+      )
+    );
+    await expect(confirmation).toContainText(
+      new RegExp(
+        `Download Signed: http:\/\/(.+?)\/pdf\/${pdfId}\/([0-9]+)\/download\/\\?expires=([0-9]+)&signature=(.+?)`
+      )
+    );
+  });
 });
