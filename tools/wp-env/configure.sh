209a5292064a8a0b9b359c9f6ba3aeed3b4eb8c6
Jake Jackson <jake@gravitypdf.com>
2025-08-26 14:18:56 +1000

    Remove unneeded configure code

--- tools/wp-env/configure.sh
+++ tools/wp-env/configure.sh
@@ -6,7 +6,3 @@ npm run wp-env run cli wp rewrite structure /%postname%/ -- --hard
 echo "Activate Plugins"
 npm run wp-env run cli wp plugin activate gravityforms gravityformspolls gravityformssurvey gravityformsquiz gravity-pdf
 npm run wp-env run tests-cli wp plugin activate gravityforms gravityformspolls gravityformssurvey gravityformsquiz gravity-pdf
-
-echo "Setup content on test site"
-npm run wp-env run tests-cli wp option add freshinstall yes
-npm run wp-env run tests-cli wp user create editor editor@test.com -- --role=editor --user_pass=password --quiet
