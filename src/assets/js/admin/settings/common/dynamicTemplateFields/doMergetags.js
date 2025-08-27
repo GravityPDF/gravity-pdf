bd1c888071a2880ad7755b8532a4798811c607a3
Jake Jackson <jake@gravitypdf.com>
2025-08-27 14:38:36 +1000

    Fix script arguments and lint

--- src/assets/js/admin/settings/common/dynamicTemplateFields/doMergetags.js
+++ src/assets/js/admin/settings/common/dynamicTemplateFields/doMergetags.js
@@ -4,5 +4,5 @@
  * @since 4.0
  */
 export function doMergetags() {
-  document.dispatchEvent( new Event( 'gform/merge_tag/initialize' ) );
+	document.dispatchEvent(new Event('gform/merge_tag/initialize'));
 }
