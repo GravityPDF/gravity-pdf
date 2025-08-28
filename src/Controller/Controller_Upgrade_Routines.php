6b722a0cdaf9b802f9b3fbe9499574eea09dabc3
Jake Jackson <jake@gravitypdf.com>
2025-08-28 10:34:29 +1000

    Housekeeping: Temp folder permission upgrade routine
    Reset the PDF tmp and mPDF tmp folder permissions back to the parent folder permission, or fallback to 755.

--- src/Controller/Controller_Upgrade_Routines.php
+++ src/Controller/Controller_Upgrade_Routines.php
@@ -55,6 +55,10 @@ class Controller_Upgrade_Routines {
 			$this->upgrade_custom_fonts();
 		}
 
+		if ( version_compare( $current_version, '6.13.2', '>=' ) && version_compare( $old_version, '6.13.2', '<' ) ) {
+			$this->fix_tmp_folder_permissions();
+		}
+
 		if ( version_compare( $current_version, '6.14.0', '>=' ) && version_compare( $old_version, '6.14.0', '<' ) ) {
 			$this->remove_legacy_update_cache();
 		}
@@ -92,6 +96,49 @@ class Controller_Upgrade_Routines {
 		}
 
 		$this->options->update_option( 'custom_fonts', $fonts );
+	}
+
+	/**
+	 * Reset temporary folders permission
+	 *
+	 * This upgrade routine will try reset all temporary folder permissions to match the parent directory,
+	 * or fallback to 755 if it cannot be read.
+	 *
+	 * @since 6.13.2
+	 */
+	protected function fix_tmp_folder_permissions() {
+		$folders = [ $this->data->template_tmp_location ];
+
+		/* If the mPDF tmp directory is moved outside the GPDF tmp directory, fix the folder permissions separately */
+		if ( strpos( $this->data->mpdf_tmp_location, $this->data->template_tmp_location ) !== 0 ) {
+			$folders[] = $this->data->mpdf_tmp_location;
+		}
+
+		foreach ( $folders as $folder ) {
+			/* Try get the folder permission from the parent directory */
+			$folder_perms = 0755;
+			$parent_dir   = dirname( $folder );
+			if ( is_dir( $parent_dir ) ) {
+				$stat         = @stat( $parent_dir ); //phpcs:ignore WordPress.PHP.NoSilencedErrors.Discouraged
+				$folder_perms = $stat ? $stat['mode'] & 0007777 : 0755;
+			}
+
+			/* Get all directories in folder */
+			$dir            = new \RecursiveDirectoryIterator( $folder, \RecursiveDirectoryIterator::SKIP_DOTS );
+			$files          = new \RecursiveCallbackFilterIterator(
+				$dir,
+				function ( $current, $key, $iterator ) {
+					return $iterator->hasChildren() || $current->isDir();
+				}
+			);
+			$files_iterator = new \RecursiveIteratorIterator( $files, \RecursiveIteratorIterator::SELF_FIRST );
+
+			/* Reset permissions on folder and all subdirectories */
+			chmod( $folder, $folder_perms ); // phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_operations_chmod
+			foreach ( $files_iterator as $file ) {
+				chmod( $file->getRealPath(), $folder_perms ); // phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_operations_chmod
+			}
+		}
 	}
 
 	/**
