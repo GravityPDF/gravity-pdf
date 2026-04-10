9c7f1e3e3762fd42be0e522486d263ebaf9df94e
Jake Jackson <jake@gravitypdf.com>
2026-04-10 15:02:55 +1000

    Improve URL Signing
    - Add redundancy to the verification process
    - Overcome vendor library issue parsing URLs params containing `[]`

--- src/Helper/Helper_Url_Signer.php
+++ src/Helper/Helper_Url_Signer.php
@@ -2,10 +2,10 @@
 
 namespace GFPDF\Helper;
 
-use DateTime;
-use GFPDF_Vendor\Spatie\UrlSigner\Exceptions\InvalidExpiration;
-use GFPDF_Vendor\Spatie\UrlSigner\Exceptions\InvalidSignatureKey;
 use GPDFAPI;
+use DateTime;
+use SodiumException;
+use Exception;
 
 /**
  * @package     Gravity PDF
@@ -32,9 +32,7 @@ class Helper_Url_Signer implements Helper_Interface_Url_Signer {
 	 * @param string $expiration
 	 *
 	 * @return string
-	 *
-	 * @throws InvalidSignatureKey
-	 * @throws InvalidExpiration
+
 	 * @since 5.2
 	 */
 	public function sign( $url, $expiration ) {
@@ -62,33 +60,57 @@ class Helper_Url_Signer implements Helper_Interface_Url_Signer {
 			GPDFAPI::update_plugin_option( 'signed_secret_token', $secret_key );
 		}
 
-		$url_signer = new Helper_Sha256_Url_Signer( $secret_key );
+		try {
+			$url_signer = new Helper_Sha256_Url_Signer( $secret_key );
 
-		/* Use default timeout if no expiration passed, or expiration is invalid */
-		if ( empty( $expiration ) || (bool) strtotime( $expiration ) === false ) {
-			$expiration = ( (int) GPDFAPI::get_plugin_option( 'logged_out_timeout', '20' ) ) . ' minutes';
+			/* Use default timeout if no expiration passed, or expiration is invalid */
+			if ( empty( $expiration ) || (bool) strtotime( $expiration ) === false ) {
+				$expiration = ( (int) GPDFAPI::get_plugin_option( 'logged_out_timeout', '20' ) ) . ' minutes';
+			}
+
+			$log->notice( 'PDF URL signing period: ' . $expiration, [ 'url' => $url ] );
+
+			$date = new DateTime();
+			$log->notice( 'PDF URL signing date: ' . $date->format( DateTime::W3C ), [ 'url' => $url ] );
+
+			$timeout = $date->modify( $expiration );
+			$log->notice( 'PDF URL signing expiration date: ' . $timeout->format( DateTime::W3C ), [ 'url' => $url ] );
+
+			/* Normalize URL to fix vendor error parsing invalid URL params */
+			$url_parts          = wp_parse_url( $url );
+			$uri_query          = $url_parts['query'] ?? '';
+			$url_parts['query'] = '_QHASH=' . base64_encode( $uri_query ); // phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_encode
+			$url                = $this->build_url( $url_parts );
+
+			/* Sign normalized URL, then extract the arguments and append to original URL */
+			$signed_url         = $url_signer->sign( $url, $timeout );
+			$signed_parts       = wp_parse_url( $signed_url );
+			$uri_query         .= str_replace( $url_parts['query'], '', $signed_parts['query'] ?? '' );
+			$url_parts['query'] = ltrim( $uri_query, '&' );
+
+			$signed_url = $this->build_url( $url_parts );
+
+			$log->notice(
+				'End PDF URL signing process',
+				[
+					'signed_url' => $signed_url,
+					'url'        => $url,
+					'expiration' => $expiration,
+				]
+			);
+
+			return $signed_url;
+		} catch ( Exception $e ) {
+			$log->error(
+				'PDF URL signing process failed',
+				[
+					'url'        => $url,
+					'expiration' => $expiration,
+				]
+			);
+
+			return $url;
 		}
-
-		$log->notice( 'PDF URL signing period: ' . $expiration, [ 'url' => $url ] );
-
-		$date = new DateTime();
-		$log->notice( 'PDF URL signing date: ' . $date->format( DateTime::W3C ), [ 'url' => $url ] );
-
-		$timeout = $date->modify( $expiration );
-		$log->notice( 'PDF URL signing expiration date: ' . $timeout->format( DateTime::W3C ), [ 'url' => $url ] );
-
-		$signed_url = $url_signer->sign( $url, $timeout );
-
-		$log->notice(
-			'End PDF URL signing process',
-			[
-				'signed_url' => $signed_url,
-				'url'        => $url,
-				'expiration' => $expiration,
-			]
-		);
-
-		return $signed_url;
 	}
 
 	/**
@@ -101,15 +123,94 @@ class Helper_Url_Signer implements Helper_Interface_Url_Signer {
 	 * @since 5.2
 	 */
 	public function verify( $url ) {
+		if ( $this->validate( $this->get_encoded_url( $url ) ) ) {
+			return true;
+		}
+
+		return $this->validate( $url );
+	}
+
+	/**
+	 * Validate the URL and return the results
+	 *
+	 * @param string $url
+	 *
+	 * @return bool
+	 *
+	 * @since 6.14.2
+	 */
+	protected function validate( $url ) {
 		$secret_key = GPDFAPI::get_plugin_option( 'signed_secret_token', '' );
 
 		try {
 			$url_signer = new Helper_Sha256_Url_Signer( $secret_key );
 
-			return $url_signer->validate( $url );
-		} catch ( InvalidSignatureKey $e ) {
-			return false;
+			/* Do legacy URL checks */
+			if ( $url_signer->validate( $url ) ) {
+				return true;
+			}
+
+			if ( $url_signer->validate( rawurldecode( $url ) ) ) {
+				return true;
+			}
+
+			/* Swap the protocol and test again. We use strtr for simultaneous replacement to avoid the sequential swap pitfall. */
+			$protocols = [
+				'https://' => 'http://',
+				'http://'  => 'https://',
+			];
+
+			if ( $url_signer->validate( strtr( $url, $protocols ) ) ) {
+				return true;
+			}
+
+			/* Strip the trailing slash right before the url param and test again */
+			$trailing_slash = [
+				'/?' => '?',
+				'?'  => '/?',
+			];
+
+			if ( $url_signer->validate( strtr( $url, $trailing_slash ) ) ) {
+				return true;
+			}
+		} catch ( Exception $e ) {
+			// do nothing
 		}
+
+		return false;
+	}
+
+	/**
+	 * Return an encoded version of the URL
+	 *
+	 * This resolves errors in the vendor URI library when processing array URL params (e.g. item[]=1)
+	 *
+	 * @param string $url
+	 *
+	 * @return string
+	 *
+	 * @since 6.14.2
+	 */
+	protected function get_encoded_url( $url ) {
+		/* Get the signature URL params */
+		$url_pieces = explode( '?', $url, 2 );
+		$base       = $url_pieces[0] ?? '';
+		$query      = $url_pieces[1] ?? '';
+
+		$qs = [];
+		wp_parse_str( $query, $qs );
+
+		$expires   = $qs['expires'] ?? '';
+		$signature = $qs['signature'] ?? '';
+
+		/* Remove the signature params, encode the query, then put it back together */
+		$delimeter = count( $qs ) > 2 ? '&' : '';
+
+		$signature_url_params = "expires={$expires}&signature={$signature}";
+		$query                = str_replace( "$delimeter$signature_url_params", '', $query );
+		$query                = '_QHASH=' . base64_encode( $query ) . '&' . $signature_url_params; // phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_encode
+
+		return implode( '?', [ $base, $query ] );
 	}
 
 	/**
@@ -122,7 +223,7 @@ class Helper_Url_Signer implements Helper_Interface_Url_Signer {
 	protected function generate_secret_key(): string {
 		try {
 			return sodium_bin2hex( sodium_crypto_secretbox_keygen() );
-		} catch ( \SodiumException $e ) {
+		} catch ( SodiumException $e ) {
 			/* Do nothing */
 		}
 
@@ -134,5 +235,27 @@ class Helper_Url_Signer implements Helper_Interface_Url_Signer {
 		}
 
 		return $password;
+	}
+
+	/**
+	 * Reverses `parse_url()`
+	 *
+	 * @param array $parsed_url
+	 *
+	 * @return string
+	 *
+	 * @since 6.14.2
+	 */
+	protected function build_url( $parsed_url ) {
+		$scheme = isset( $parsed_url['scheme'] ) ? $parsed_url['scheme'] . '://' : '';
+		$host   = $parsed_url['host'] ?? '';
+		$port   = isset( $parsed_url['port'] ) ? ':' . $parsed_url['port'] : '';
+		$user   = $parsed_url['user'] ?? '';
+		$pass   = isset( $parsed_url['pass'] ) ? ':' . $parsed_url['pass'] : '';
+		$pass   = ( $user || $pass ) ? "$pass@" : '';
+		$path   = $parsed_url['path'] ?? '';
+		$query  = isset( $parsed_url['query'] ) ? '?' . $parsed_url['query'] : '';
+
+		return urldecode( "$scheme$user$pass$host$port$path$query" );
 	}
 }
